cea0b8d71ff25fb4db49cc2454536b775269e85a
mamutal91 <mamutal91@gmail.com>
2018-12-22 00:14:13 -0200

    Autocommit Git-Cron: remove '.mamutal91/config.sh' remove '.mamutal91/files/du_strings.xml' remove '.mamutal91/grub.sh' remove '.mamutal91/hostname' remove '.mamutal91/pacotes.sh' add '.mamutal91/ArchLinux/config.sh' add '.mamutal91/ArchLinux/grub.sh' add '.mamutal91/ArchLinux/pacotes.sh' add '.mamutal91/DirtyUnicorns/du_strings.xml' add '.mamutal91/DirtyUnicorns/sync.sh'

---
+++ .mamutal91/DirtyUnicorns/sync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# github.com/mamutal91
+
+repo sync -f --force-sync
+
+# Hardware Interfaces ( correção do hotspot )
+rm -rf hardware/interfaces
+git clone https://github.com/AOSiP/platform_hardware_interfaces.git -b pie hardware/interfaces
+
+# Correção Telephony
+rm -rf frameworks/opt/telephony
+git clone https://github.com/AOSiP/platform_frameworks_opt_telephony.git -b pie frameworks/opt/telephony
+
+# Tradução DU-Teaks
+rm -rf packages/apps/DU-Tweaks/res/values-pt-rBR/du_strings.xml
+mkdir -p packages/apps/DU-Tweaks/res/values-pt-rBR
+cd packages/apps/DU-Tweaks/res/values-pt-rBR/
+wget https://raw.githubusercontent.com/mamutal91/dotfiles/master/.mamutal91/DirtyUnicorns/du_strings.xml
+cd $DU
