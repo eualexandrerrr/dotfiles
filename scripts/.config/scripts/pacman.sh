ff4e900354cbc0140ab841b8ef0e61d2f552b0b1
mamutal91 <mamutal91@gmail.com>
2018-12-22 05:04:17 -0200

    Autocommit Git-Cron: add 'scripts/.config/scripts/pacman.sh'

--- scripts/.config/scripts/pacman.sh
+++ scripts/.config/scripts/pacman.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 # github.com/mamutal91
 
-echo :::::::::::::::::::::::: Encontrando melhores mirrors
-sudo reflector -l 10 --sort rate --save /etc/pacman.d/mirrorlist
+echo :::::::::::::::::::::::: Atualizando Pacman
+sleep 1s
+    sudo pacman -Syu --noconfirm
 
-echo :::::::::::::::::::::::: Atualizando Pacman ;
-sleep 1s ;
-    sudo pacman -Syu --noconfirm;
+echo :::::::::::::::::::::::: Atualizando AUR
+sleep 1s
+    yay -Syyu --noconfirm
 
-echo :::::::::::::::::::::::: Atualizando AUR ;
-sleep 1s ;
-    yay -Syyu --noconfirm;
+echo :::::::::::::::::::::::: Removendo pacotes Pacman não utilizados
+sleep 1s 
+    sudo pacman -Qdtq
+    sudo pacman -Rns $(pacman -Qdtq) --noconfirm
 
-echo :::::::::::::::::::::::: Removendo pacotes Pacman não utilizados ;
-sleep 1s ;
-    sudo pacman -Qdtq;
-    sudo pacman -Rns $(pacman -Qdtq) --noconfirm;
+echo :::::::::::::::::::::::: Removendo pacotes AUR não utilizados
+sleep 1s
+    yay -Qdtq
+    yay -R $(yay -Qdtq) --noconfirm
 
-echo :::::::::::::::::::::::: Removendo pacotes AUR não utilizados ;
-sleep 1s ;
-    yay -Qdtq;
-    yay -R $(yay -Qdtq) --noconfirm;
-
-canberra-gtk-play --file=$HOME/.local/share/sounds/concluido.wav 
+canberra-gtk-play --file=$HOME/.local/share/sounds/completed.wav 
