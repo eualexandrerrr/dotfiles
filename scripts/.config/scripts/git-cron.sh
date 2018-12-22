9f8d5ca420840f9cef3ed5fc3cffcc2cb7881b14
mamutal91 <mamutal91@gmail.com>
2018-12-22 14:47:48 -0200

    Autocommit Git-Cron: add 'scripts/.config/scripts/git-cron.sh'

--- scripts/.config/scripts/git-cron.sh
+++ scripts/.config/scripts/git-cron.sh
@@ -21,7 +21,7 @@ atualiza() {
 			if [ ! -z "$status" ]; then
 			c=$(echo $(git add . -n | tr '\r\n' ' '))
 			m="Autocommit Git-Cron: $c"
-			DISPLAY=:0 notify-send "Git-Cron" "Gerando commits <b>$(basename $1)</b>."
+			DISPLAY=:0 notify-send "Git-Cron" "Gerando commits para <b>$(basename $1)</b>."
 			git add .
 			git commit -m "$m"
 			git push
