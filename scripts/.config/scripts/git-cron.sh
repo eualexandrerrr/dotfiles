baedf81386a5e1121518d6273ff5222d3b0bd41f
mamutal91 <mamutal91@gmail.com>
2018-12-22 18:01:08 -0200

    Autocommit Git-Cron: add 'scripts/.config/scripts/git-cron.sh'

--- scripts/.config/scripts/git-cron.sh
+++ scripts/.config/scripts/git-cron.sh
@@ -24,7 +24,7 @@ atualiza() {
 			git add .
 			git commit -m "$m"
 			git push
-			notify-send "Git-Cron" "Repo <b>$(basename $1)</b> atualizado."
+			DISPLAY=:0 notify-send "Git-Cron" "Repo <b>$(basename $1)</b> atualizado."
 			fi
 		fi
 	fi
