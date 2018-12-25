9ea26c1b778322f6b04e7b9e4a9480a4058f1c9b
mamutal91 <mamutal91@gmail.com>
2018-12-25 14:37:00 -0200

    Autocommit Git-Cron: add 'home/.xinitrc' add 'polybar/.config/polybar/config' add 'polybar/.config/polybar/launch.sh'

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,18 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar (depending on multiple monitors or not)
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload cima &
-		MONITOR=$m polybar --reload baixo &
-	done
-else
-	polybar cima &
-	polybar baixo &
-fi
+# Launch bar1 and bar2
+polybar cima &
+#polybar baixo &
+
+echo "Bars launched..."
