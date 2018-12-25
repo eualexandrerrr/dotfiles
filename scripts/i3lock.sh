a4209058585229e289e0832df31f399518db94d9
mamutal91 <mamutal91@gmail.com>
2018-12-25 16:43:44 -0200

    Autocommit Git-Cron: add 'i3/config' add 'scripts/git-cron.sh' add 'scripts/i3lock.sh' add 'scripts/pywal.sh' add 'scripts/update.sh'

---
+++ scripts/i3lock.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# github.com/mamutal91
+
+scr='/tmp/screenshot.png'
+icon="$HOME/.local/share/i3lock/i3lock.png"
+font="$HOME/.local/share/fonts/OpenSans.ttf"
+
+[ ! -d $HOME/.local/share/i3lock/ ] && mkdir -p $HOME/.local/share/i3lock/
+[ ! -f $icon ] && curl -s -L 'https://i.imgur.com/XbxRMX7.png' > $icon
+
+gradientColor='#282a36'
+
+# Screenshot para plano de fundo do i3lock
+maim "$scr"
+
+# Obtenha dimensões gradientes diretamente da captura de tela
+read width height <<<$(file $scr | cut -d, -f 2 | tr -d ' ' | tr 'x' ' ')
+height=$((height / 2))
+
+convert "$scr" -scale 10% -scale 1000%\
+	-size "${width}x${height}" -gravity south-west \
+	gradient:none-"$gradientColor" -composite -matte \
+	"$icon" -gravity center -composite -matte \
+	-gravity center -pointsize 20 \
+	-font $font -fill "#EAE4D1" -annotate +0+200 "$fortune" "$scr"
+i3lock -ui "$scr"
