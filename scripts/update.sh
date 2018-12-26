9646276edf92b6893fdfb15f604e72f5dfd21cd2
mamutal91 <mamutal91@gmail.com>
2018-12-26 00:10:01 -0200

    Autocommit Git-Cron: add 'scripts/update.sh'

--- scripts/update.sh
+++ scripts/update.sh
@@ -44,4 +44,5 @@ canberra-gtk-play --file=$HOME/.local/share/sounds/completed.wav
 sleep 1s
 i3-msg restart
 notify-send "Atualização concluída com sucesso"
+sleep 1s
 cd $HOME/.config/polybar && ./launch.sh
