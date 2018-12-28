5a626633aef8437bb3e906d7a62852c248a12992
mamutal91 <mamutal91@gmail.com>
2018-12-28 07:58:01 -0200

    Autocommit Git-Cron: remove 'home/.Xresources' remove 'home/.local/share/fonts/OpenSans.ttf' remove 'home/.local/share/i3lock/i3lock.png' remove 'home/.local/share/sounds/completed.wav' remove 'home/.local/share/sounds/screencast-start.wav' remove 'home/.local/share/sounds/screencast-stop.wav' remove 'home/.local/share/sounds/screenshot.wav' remove 'home/.local/share/wallpapers/binary.png' remove 'home/.local/share/wallpapers/firefox.png' remove 'home/.nvidia-xinitrc' remove 'home/.xinitrc' remove 'home/.zlogin' remove 'home/.zshrc' add 'scripts/update.sh' add 'home/.files/.Xresources' add 'home/.files/.local/share/fonts/OpenSans.ttf' add 'home/.files/.local/share/i3lock/i3lock.png' add 'home/.files/.local/share/sounds/completed.wav' add 'home/.files/.local/share/sounds/screencast-start.wav' add 'home/.files/.local/share/sounds/screencast-stop.wav' add 'home/.files/.local/share/sounds/screenshot.wav' add 'home/.files/.local/share/wallpapers/binary.png' add 'home/.files/.local/share/wallpapers/firefox.png' add 'home/.files/.nvidia-xinitrc' add 'home/.files/.xinitrc' add 'home/.files/.zlogin' add 'home/.files/.zshrc'

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,11 +8,11 @@ configs="${HOME}/.config"
 
 cd $dotfiles
 
-    sudo rm -rf $HOME/.local/share/{fonts,i3lock,sounds,wallpapers}
-    sudo cp -r home/.local/ $HOME
+    sudo rm -rf $HOME/.files/.local/share/{fonts,i3lock,sounds,wallpapers}
+    sudo cp -r home/.files/.local/ $HOME
 
-    sudo rm -rf $HOME/{.zshrc,.Xresources,.xinitrc,.nvidia-xinitrc,.zlogin}
-    sudo cp -r home/{.zshrc,.Xresources,.xinitrc,.nvidia-xinitrc,.zlogin} $HOME
+    sudo rm -rf $HOME/.files/{.zshrc,.Xresources,.xinitrc,.nvidia-xinitrc,.zlogin}
+    sudo cp -r home/.files/{.zshrc,.Xresources,.xinitrc,.nvidia-xinitrc,.zlogin} $HOME
 
     sudo rm -rf ${configs}/compton && sudo cp -r compton ${configs}
     sudo rm -rf ${configs}/dunst && sudo cp -r dunst ${configs}
