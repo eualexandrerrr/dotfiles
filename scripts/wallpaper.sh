b4f6f8bfcdb0661c5d1dd974e3e0a34685f3e2c3
mamutal91 <mamutal91@gmail.com>
2018-12-28 13:10:58 -0200

    Autocommit Git-Cron: add 'scripts/wallpaper.sh'

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # github.com/mamutal91
 
-wpp=$HOME/.local/share/wallpapers/city.png
+#wpp=$HOME/.local/share/wallpapers/city.png
+wpp=https://raw.githubusercontent.com/mamutal91/stojects/master/wallpapers/teste.png
 
 wal -i $wpp -o wal-set
 #feh --bg-scale $wpp
