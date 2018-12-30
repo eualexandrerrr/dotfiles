0daf28f0d702126e9a17369605722c44737d0b42
mamutal91 <mamutal91@gmail.com>
2018-12-30 17:48:59 -0200

    Autocommit Git-Cron: add 'scripts/update.sh'

--- scripts/update.sh
+++ scripts/update.sh
@@ -35,9 +35,9 @@ function mamutal91configs(){
     sudo rm -rf /etc/X11/nvidia-xorg.conf.d/30-nvidia.conf
     sudo rm -rf /etc/modules-load.d/bbswitch.conf
     sudo rm -rf /etc/modprobe.d/bbswitch.conf
-    sudo cp -r ${dotfiles}/system/.files/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
-    sudo cp -r ${dotfiles}/system/.files/etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
-    sudo cp -r ${dotfiles}/system/.files/etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
+#    sudo cp -r ${dotfiles}/system/.files/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
+#    sudo cp -r ${dotfiles}/system/.files/etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
+#    sudo cp -r ${dotfiles}/system/.files/etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
 }
 [[ $USER == "mamutal91" ]] && mamutal91configs || echo No
 
