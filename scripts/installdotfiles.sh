71cfd6f869c28c185dbd970b07e5b7183d7efec8
mamutal91 <mamutal91@gmail.com>
2019-01-01 23:26:37 -0200

    Autocommit Git-Cron: add 'scripts/installdotfiles.sh'

--- scripts/installdotfiles.sh
+++ scripts/installdotfiles.sh
@@ -3,10 +3,8 @@
 
 # sh -c "$(wget http://bit.ly/mamutal91DOTFILES -O -)"
 
-sudo -s
-
 cd $HOME
-rm -rf githubteste
+sudo rm -rf githubteste
 mkdir githubteste && cd githubteste
 git clone https://github.com/mamutal91/dotfiles && cd dotfiles
 
