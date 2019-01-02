6da07ee4f091823d99601d473e50294a39e5a3d9
mamutal91 <mamutal91@gmail.com>
2019-01-02 02:15:01 -0200

    Autocommit Git-Cron: add 'scripts/.config/scripts/stow.sh'

--- scripts/.config/scripts/stow.sh
+++ scripts/.config/scripts/stow.sh
@@ -13,21 +13,22 @@ stow rofi
 stow scripts
 stow termite
 stow thunar
+stow etc
 
 function mamutal91configs(){
     sudo rm -rf /etc/X11/xorg.conf.d/10-evdev.conf
     sudo rm -rf /etc/X11/xorg.conf.d/20-intel.conf
     sudo rm -rf /etc/X11/xorg.conf.d/30-touchpad.conf
-    sudo cp -r etc/X11/xorg.conf.d/10-evdev.conf /etc/X11/xorg.conf.d/
-    sudo cp -r etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/
-    sudo cp -r etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
+#    sudo cp -r etc/X11/xorg.conf.d/10-evdev.conf /etc/X11/xorg.conf.d/
+#    sudo cp -r etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/
+#    sudo cp -r etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
 
     sudo rm -rf /etc/X11/nvidia-xorg.conf.d/30-nvidia.conf
     sudo rm -rf /etc/modules-load.d/bbswitch.conf
     sudo rm -rf /etc/modprobe.d/bbswitch.conf
-    sudo cp -r etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
-    sudo cp -r etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
-    sudo cp -r etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
+#    sudo cp -r etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
+#    sudo cp -r etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
+#    sudo cp -r etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
 }
 [[ $USER == "mamutal91" ]] && mamutal91configs || echo No
 
