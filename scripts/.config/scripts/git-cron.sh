0db57994de5804e8aaf5dcc8e6b02bd816196083
mamutal91 <mamutal91@gmail.com>
2019-01-02 01:25:07 -0200

    Autocommit Git-Cron: add 'scripts/.config/scripts/git-cron.sh'

--- scripts/.config/scripts/git-cron.sh
+++ scripts/.config/scripts/git-cron.sh
@@ -9,16 +9,11 @@ export EDITOR="nano"
 git config --global user.name "mamutal91" 
 git config --global user.email "mamutal91@gmail.com"
 
-dir="${HOME}"
-repos=('.dotfiles')
-remoto="mamutal91@archlinux"
+cd $HOME/.dotfiles
 
 atualiza() {
-	if [ -d $1 ]; then
-		if [ ! -f $1/.noup ]; then
-			cd $1
-			status=$(git add . -n)
-			if [ ! -z "$status" ]; then
+	status=$(git add . -n)
+		if [ ! -z "$status" ]; then
 			c=$(echo $(git add . -n | tr '\r\n' ' '))
 			m="Autocommit Git-Cron: $c"
 			DISPLAY=:0 notify-send "Git-Cron Commits" "$(basename $1)"
@@ -26,20 +21,7 @@ atualiza() {
 			git commit -m "$m"
 			git push
 			DISPLAY=:0 notify-send "Git-Cron Push" "$(basename $1) atualizado."
-			fi
 		fi
-	fi
 }
 
-if [ ! $1 ] || [ $1 == "-a" ]; then
-	for r in "${repos[@]}";	do
-		caminho="${dir}/${r}"
-		atualiza "$caminho"
-	done
-
-	atualiza "${HOME}"
-	[ "$1" == "-a" ] && ssh $remoto "/usr/local/scripts/git-http"
-else
-	caminho="$@"
-	atualiza "$caminho"
-fi
+atualiza
