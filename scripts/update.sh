d5457248220400d4495757abf59874bb1037d726
mamutal91 <mamutal91@gmail.com>
2019-01-02 00:11:57 -0200

    Autocommit Git-Cron: add 'scripts/update.sh' remove 'system/.files/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf' remove 'system/.files/etc/X11/xorg.conf.d/10-evdev.conf' remove 'system/.files/etc/X11/xorg.conf.d/20-intel.conf' remove 'system/.files/etc/X11/xorg.conf.d/30-touchpad.conf' remove 'system/.files/etc/modprobe.d/bbswitch.conf' remove 'system/.files/etc/modules-load.d/bbswitch.conf' add 'system/.config/.files/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf' add 'system/.config/.files/etc/X11/xorg.conf.d/10-evdev.conf' add 'system/.config/.files/etc/X11/xorg.conf.d/20-intel.conf' add 'system/.config/.files/etc/X11/xorg.conf.d/30-touchpad.conf' add 'system/.config/.files/etc/modprobe.d/bbswitch.conf' add 'system/.config/.files/etc/modules-load.d/bbswitch.conf'

--- scripts/update.sh
+++ scripts/update.sh
@@ -28,16 +28,16 @@ function mamutal91configs(){
     sudo rm -rf /etc/X11/xorg.conf.d/10-evdev.conf
     sudo rm -rf /etc/X11/xorg.conf.d/20-intel.conf
     sudo rm -rf /etc/X11/xorg.conf.d/30-touchpad.conf
-    sudo cp -r ${dotfiles}/system/.files/etc/X11/xorg.conf.d/10-evdev.conf /etc/X11/xorg.conf.d/
-    sudo cp -r ${dotfiles}/system/.files/etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/
-    sudo cp -r ${dotfiles}/system/.files/etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
+    sudo cp -r ${dotfiles}/system/.config/.files/etc/X11/xorg.conf.d/10-evdev.conf /etc/X11/xorg.conf.d/
+    sudo cp -r ${dotfiles}/system/.config/.files/etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/
+    sudo cp -r ${dotfiles}/system/.config/.files/etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
 
     sudo rm -rf /etc/X11/nvidia-xorg.conf.d/30-nvidia.conf
     sudo rm -rf /etc/modules-load.d/bbswitch.conf
     sudo rm -rf /etc/modprobe.d/bbswitch.conf
-    sudo cp -r ${dotfiles}/system/.files/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
-    sudo cp -r ${dotfiles}/system/.files/etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
-    sudo cp -r ${dotfiles}/system/.files/etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
+    sudo cp -r ${dotfiles}/system/.config/.files/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
+    sudo cp -r ${dotfiles}/system/.config/.files/etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
+    sudo cp -r ${dotfiles}/system/.config/.files/etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
 }
 [[ $USER == "mamutal91" ]] && mamutal91configs || echo No
 
