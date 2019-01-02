f95ceea8312c54084921f6eb72e83cea1c1c8b7f
mamutal91 <mamutal91@gmail.com>
2019-01-02 02:25:01 -0200

    Autocommit Git-Cron: add 'i3/.config/i3/config' add 'home/bin/git-cron-dotfiles.sh' add 'home/bin/git-cron.sh' add 'home/bin/i3lock.sh' add 'home/bin/installdotfiles.sh' add 'home/bin/menu-power.sh' add 'home/bin/pacman.sh' add 'home/bin/screencast.sh' add 'home/bin/screenshot.sh' add 'home/bin/spotify.sh' add 'home/bin/stow.sh' add 'home/bin/update.sh' add 'home/bin/wallpaper.sh'

---
+++ home/bin/pacman.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# github.com/mamutal91
+
+(cat ~/.cache/wal/sequences &)
+
+echo Atualizando Pacman
+sleep 1s
+    sudo pacman -Syu --noconfirm
+
+echo Atualizando AUR
+sleep 1s
+    yay -Syyu --noconfirm
+
+echo Removendo pacotes Pacman não utilizados
+sleep 1s 
+    sudo pacman -Qdtq
+    sudo pacman -Rns $(pacman -Qdtq) --noconfirm
+
+echo Removendo pacotes AUR não utilizados
+sleep 1s
+    yay -Qdtq
+    yay -R $(yay -Qdtq) --noconfirm
+
+canberra-gtk-play --file=$HOME/.local/share/sounds/completed.wav 
