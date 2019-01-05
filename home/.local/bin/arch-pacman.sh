98dc0f32f9b9eebcade9a08ae5f0dd54b412ec80
mamutal91 <mamutal91@gmail.com>
2019-01-05 13:02:32 -0200

    Autocommit Git-Cron: add 'home/.local/bin/arch-pacman.sh'

--- home/.local/bin/arch-pacman.sh
+++ home/.local/bin/arch-pacman.sh
@@ -3,20 +3,20 @@
 
 (cat ~/.cache/wal/sequences &)
 
-echo Atualizando Pacman
+echo ▆▆ Atualizando Pacman
 sleep 1s
     sudo pacman -Syu --noconfirm
 
-echo Atualizando AUR
+echo ▆▆ Atualizando AUR
 sleep 1s
     yay -Syyu --noconfirm
 
-echo Removendo pacotes Pacman não utilizados
+echo ▆▆ Removendo pacotes Pacman não utilizados
 sleep 1s 
     sudo pacman -Qdtq
     sudo pacman -Rns $(pacman -Qdtq) --noconfirm
 
-echo Removendo pacotes AUR não utilizados
+echo ▆▆ Removendo pacotes AUR não utilizados
 sleep 1s
     yay -Qdtq
     yay -R $(yay -Qdtq) --noconfirm
