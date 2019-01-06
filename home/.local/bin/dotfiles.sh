051e8edf41e470a2fb6734c270acd07bf1c61c18
mamutal91 <mamutal91@gmail.com>
2019-01-06 19:10:01 -0200

    Autocommit Git-Cron: add 'home/.local/bin/dotfiles.sh'

--- home/.local/bin/dotfiles.sh
+++ home/.local/bin/dotfiles.sh
@@ -22,18 +22,14 @@ stow termite
 }
 
 function mamutal91-configs(){
-    sudo rm -rf /etc/X11/xorg.conf.d/10-evdev.conf
-    sudo rm -rf /etc/X11/xorg.conf.d/20-intel.conf
-    sudo rm -rf /etc/X11/xorg.conf.d/30-touchpad.conf
-    sudo cp -r etc/X11/xorg.conf.d/10-evdev.conf /etc/X11/xorg.conf.d/
-    sudo cp -r etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/
-    sudo cp -r etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
-
-    sudo rm -rf /etc/X11/nvidia-xorg.conf.d/30-nvidia.conf
-    sudo cp -r etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
+    sudo rm -rf /etc/X11/
+    sudo cp -r etc/X11/ /etc/
 
     sudo rm -rf /etc/modules-load.d/bbswitch.conf
     sudo rm -rf /etc/modprobe.d/bbswitch.conf
+
+    sudo mkdir -p /etc/modules-load.d/
+    sudo mkdir -p /etc/modprobe.d/
     sudo cp -r etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
     sudo cp -r etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
 }
