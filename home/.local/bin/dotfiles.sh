1b1291bf4c4e411ec8b49c461e42d81b4e7cf777
mamutal91 <mamutal91@gmail.com>
2019-01-06 20:08:31 -0200

    Autocommit Git-Cron: add 'home/.local/bin/dotfiles.sh'

--- home/.local/bin/dotfiles.sh
+++ home/.local/bin/dotfiles.sh
@@ -24,7 +24,8 @@ stow termite
 function mamutal91-configs(){
     sudo rm -rf /etc/X11/xorg.conf.d/10-evdev.conf
     sudo rm -rf /etc/X11/xorg.conf.d/30-touchpad.conf
-    sudo cp -r etc/X11/xorg.conf.d/10-evdev.conf /etc/xorg.conf.d/
+    sudo cp -r etc/X11/xorg.conf.d/10-evdev.conf /etc/X11/xorg.conf.d/
+    sudo cp -r etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
 }
 
 function mamutal91(){
