365f057f50863464805f62b63a341fc9d71d7aa3
mamutal91 <mamutal91@gmail.com>
2019-01-06 20:28:17 -0200

    Autocommit Git-Cron: add 'i3/.config/i3/config' add 'home/.local/bin/arch-pacotes.sh'

---
+++ home/.local/bin/arch-pacotes.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# github.com/mamutal91
+
+(cat ~/.cache/wal/sequences &)
+
+USUARIO=mamutal91
+
+sudo pacman -Sy
+
+readonly PACOTES_PACMAN=(
+    git
+    i3-gaps i3lock compton dunst rofi mpd maim neofetch scrot lxappearance feh gpicview python-pywal python-setuptools zsh openssh cronie plasma-browser-integration
+    pulseaudio pulseeffects pulsemixer
+    termite terminus-font
+    telegram-desktop
+    code pinta vlc gparted qbittorrent galculator
+    libreoffice-fresh libreoffice-fresh-pt-br
+    firefox firefox-i18n-pt-br
+    thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman 
+    steam ttf-liberation
+    winetricks
+    nvidia nvidia-settings nvidia-utils lib32-virtualgl lib32-nvidia-utils bumblebee mesa primus
+#   opencl-nvidia lib32-libvdpau lib32-opencl-nvidia
+#   xf86-video-intel
+    xorg-server xorg-xrandr xorg-xblacklight xorg-xinit xorg-xprop xautolock xclip
+    linux-headers networkmanager numlockx gvfs unrar unzip wget)
+
+readonly PACOTES_AUR=(
+    nvidia-xrun
+    polybar jsoncpp
+    arc-gtk-theme paper-icon-theme-git capitaine-cursors
+    ttf-dejavu ttf-font-awesome 
+    rambox-bin spotify grive-git)
+
+function instalar_pacotes_pacman(){
+    for i in "${PACOTES_PACMAN[@]}"; do
+        sudo pacman -S ${i} --needed --noconfirm
+    done 
+}
+
+function instalar_pacotes_aur(){
+    for i in "${PACOTES_AUR[@]}"; do
+        yay -S ${i} --needed --noconfirm
+    done 
+}
+
+function mirror(){
+    sudo pacman -Sy reflector --needed --noconfirm
+    sudo reflector -c Brazil --save /etc/pacman.d/mirrorlist
+}
+
+function winetricks(){
+    winetricks directx9 vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 dotnet40 dotnet452 vb6 xact xna31 xna40 msl31 openal corefonts
+}
+
+function configurar_sistema(){
+    sudo systemctl enable NetworkManager
+    sudo systemctl enable cronie
+    sudo systemctl enable bumblebeed.service
+    sudo chown -R $USUARIO:$USUARIO /home/$USUARIO
+    sudo gpasswd -a $USUARIO bumblebee
+}
+
+#mirror
+#winetricks
+
+instalar_pacotes_pacman
+instalar_pacotes_aur
+configurar_sistema
