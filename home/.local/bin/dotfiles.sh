530c993af30f99c95687a174f24dabd9be46518f
mamutal91 <mamutal91@gmail.com>
2019-01-06 22:34:27 -0200

    Autocommit Git-Cron: add 'home/.local/bin/dotfiles.sh' remove 'nvidia/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf' remove 'nvidia/etc/X11/xorg.conf.d/20-intel.conf' remove 'nvidia/etc/modprobe.d/bbswitch.conf' remove 'nvidia/etc/modules-load.d/bbswitch.conf' add 'etc/nvidia/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf' add 'etc/nvidia/etc/X11/xorg.conf.d/20-intel.conf' add 'etc/nvidia/etc/modprobe.d/bbswitch.conf' add 'etc/nvidia/etc/modules-load.d/bbswitch.conf'

--- home/.local/bin/dotfiles.sh
+++ home/.local/bin/dotfiles.sh
@@ -21,6 +21,7 @@ stow termite
 }
 
 function xorg(){
+    # XORGS de teclado e mouse
     sudo rm -rf /etc/X11/xorg.conf.d/10-evdev.conf
     sudo rm -rf /etc/X11/xorg.conf.d/30-touchpad.conf
 
@@ -29,22 +30,32 @@ function xorg(){
     sudo cp -r etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/
 }
 
+
 function xorg-nvidia(){
-sudo rm -rf /etc/X11/xorg.conf.d/20-intel.conf
+    # XORG da INTEL
+    sudo rm -rf /etc/X11/xorg.conf.d/20-intel.conf
 
-sudo mkdir -p /etc/X11/xorg.conf.d/
-sudo cp -r nvidia/etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/
+    sudo mkdir -p /etc/X11/xorg.conf.d/
+    sudo cp -r etc/nvidia/etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/
 
-sudo rm -rf /etc/X11/nvidia-xorg.conf.d/30-nvidia.conf
-sudo rm -rf /etc/modules-load.d/bbswitch.conf
-sudo rm -rf /etc/modprobe.d/bbswitch.conf
+    # XORG da NVIDIA e BBSWITCH
+    sudo rm -rf /etc/X11/nvidia-xorg.conf.d/30-nvidia.conf
+    sudo rm -rf /etc/modules-load.d/bbswitch.conf
+    sudo rm -rf /etc/modprobe.d/bbswitch.conf
 
-sudo mkdir -p /etc/modules-load.d/
-sudo mkdir -p /etc/modprobe.d/
-sudo mkdir -p /etc/X11/nvidia-xorg.conf.d/
-sudo cp -r nvidia/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
-sudo cp -r nvidia/etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
-sudo cp -r nvidia/etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
+    sudo mkdir -p /etc/modules-load.d/
+    sudo mkdir -p /etc/modprobe.d/
+    sudo mkdir -p /etc/X11/nvidia-xorg.conf.d/
+    sudo cp -r etc/nvidia/etc/X11/nvidia-xorg.conf.d/30-nvidia.conf /etc/X11/nvidia-xorg.conf.d/
+    sudo cp -r etc/nvidia/etc/modules-load.d/bbswitch.conf /etc/modules-load.d/
+    sudo cp -r etc/nvidia/etc/modprobe.d/bbswitch.conf /etc/modprobe.d/
+
+    # XORGS de teclado e mouse para NVIDIA
+    sudo rm -rf /etc/X11/nvidia-xorg.conf.d/10-evdev.conf
+    sudo rm -rf /etc/X11/nvidia-xorg.conf.d/30-touchpad.conf
+
+    sudo cp -r etc/X11/xorg.conf.d/10-evdev.conf /etc/X11/nvidia-xorg.conf.d/
+    sudo cp -r etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/nvidia-xorg.conf.d/
 }
 
 function mamutal91(){
