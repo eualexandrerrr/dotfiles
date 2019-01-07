5d5b548770dcd56e7e5ac6e7bfb485d50b518cf3
mamutal91 <mamutal91@gmail.com>
2019-01-07 00:45:01 -0200

    Autocommit Git-Cron: add 'home/.local/bin/bateria.sh'

--- home/.local/bin/bateria.sh
+++ home/.local/bin/bateria.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 # github.com/mamutal91
 
-THRESHOLD=35 # percent
-
-lock_path='/tmp/batmon.lock'
-
-lockfile -r 0 $lock_path 2>/dev/null || exit
-
-acpi_path=$(find /sys/class/power_supply/ -name 'BAT*' | head -1)
-charge_now=$(cat "$acpi_path/charge_now")
-charge_full=$(cat "$acpi_path/charge_full")
-charge_status=$(cat "$acpi_path/status")
-charge_percent=$(printf '%.0f' $(echo "$charge_now / $charge_full * 100" | bc -l))
-message="Battery running critically low at $charge_percent%!"
-
-if [[ $charge_status == 'Discharging' ]] && [[ $charge_percent -le $THRESHOLD ]]; then
-  DISPLAY=:0 notify-send "Bateria descarregando."
+BATTINFO=`acpi -b`
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
+    DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
 fi
-
-rm -f $lock_path
