d0608bc1371e84b502589bf119e2643329027359
mamutal91 <mamutal91@gmail.com>
2019-01-11 01:40:01 -0200

    Autocommit Git-Cron: add 'home/.local/bin/cat.sh' remove 'i3/.config/i3/config' add 'i3/.config/i3/config1'

--- home/.local/bin/cat.sh
+++ home/.local/bin/cat.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # github.com/mamutal91
 
-sudo -s
-cat /home/mamutal91/github/dotfiles/i3/.config/i3/config \
-    /home/mamutal91/github/dotfiles/i3/.config/i3/rules > /home/mamutal91/.config/i3/config
+cd /home/mamutal91/github/dotfiles/i3/.config/i3/
+
+cat config1 \
+    rules > /home/mamutal91/.config/i3/config
 #exec /usr/bin/i3
