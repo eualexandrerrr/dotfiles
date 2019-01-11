3700cd050a6f2d825ea101366f965cd1633eb283
mamutal91 <mamutal91@gmail.com>
2019-01-11 02:14:09 -0200

    Autocommit Git-Cron: add 'home/.local/bin/i3gen.sh' remove 'i3/.config/i3/rules' add 'i3/.config/i3/rules/config'

--- home/.local/bin/i3gen.sh
+++ home/.local/bin/i3gen.sh
@@ -4,7 +4,7 @@
 cd $HOME/github/dotfiles/i3/.config/i3/
 
 cat config \
-    rules > /tmp/i3config
+    rules/config > /tmp/i3config
 
 sudo rm -rf $HOME/.config/i3/config
 sudo mv /tmp/i3config $HOME/.config/i3/config
