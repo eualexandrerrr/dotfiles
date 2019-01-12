111ca1f82b53eccfb3f71bed1af5f0facf9a12d2
mamutal91 <mamutal91@gmail.com>
2019-01-12 13:23:24 -0200

    Autocommit Git-Cron: add 'home/.local/bin/git-cron.sh'

--- home/.local/bin/git-cron.sh
+++ home/.local/bin/git-cron.sh
@@ -8,7 +8,7 @@ git config --global user.name "mamutal91"
 git config --global user.email "mamutal91@gmail.com"
 
 dir="${HOME}/github"
-repos=('archlinux' 'dotfiles' 'mamutal91.github.io' 'dirtyunicorns' 'strojects')
+repos=('archlinux' 'dotfiles' 'dirtyunicorns' 'mamutal91.github.io')
 remoto="mamutal91@archlinux"
 
 atualiza() {
