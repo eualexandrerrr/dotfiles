66d46701a8991bcc960bd6a44f333df1d19def6b
mamutal91 <mamutal91@gmail.com>
2019-01-14 00:08:01 -0200

    git-cron-dotfiles: Upload git-cron-dotfiles

---
+++ home/.local/bin/git-cron-dotfiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# github.com/mamutal91
+
+dir="${HOME}/github"
+repos=('dotfiles')
+remoto="mamutal91@archlinux"
+
+atualiza() {
+	if [ -d $1 ]; then
+		if [ ! -f $1/.noup ]; then
+			cd $1
+			status=$(git add . -n)
+			if [ ! -z "$status" ]; then
+			git add .
+			git commit
+			git push
+			fi
+		fi
+	fi
+}
+
+if [ ! $1 ] || [ $1 == "-a" ]; then
+	for r in "${repos[@]}";	do
+		caminho="${dir}/${r}"
+		atualiza "$caminho"
+	done
+
+	atualiza "${HOME}/github"
+	[ "$1" == "-a" ] && ssh $remoto "/usr/local/scripts/git-http"
+else
+	caminho="$@"
+	atualiza "$caminho"
+fi
