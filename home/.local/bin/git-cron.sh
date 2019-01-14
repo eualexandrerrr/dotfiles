1210ac9839b25e3b0947fb1ffd7ed5ce5325f29a
mamutal91 <mamutal91@gmail.com>
2019-01-13 22:07:36 -0200

    Updates on git-cron

--- home/.local/bin/git-cron.sh
+++ home/.local/bin/git-cron.sh
@@ -8,7 +8,7 @@ git config --global user.name "mamutal91"
 git config --global user.email "mamutal91@gmail.com"
 
 dir="${HOME}/github"
-repos=('archlinux' 'dotfiles' 'dirtyunicorns' 'mamutal91.github.io')
+repos=('archlinux' 'dotfiles' 'backups' 'dirtyunicorns' 'mamutal91.github.io')
 remoto="mamutal91@archlinux"
 
 atualiza() {
@@ -17,13 +17,9 @@ atualiza() {
 			cd $1
 			status=$(git add . -n)
 			if [ ! -z "$status" ]; then
-			c=$(echo $(git add . -n | tr '\r\n' ' '))
-			m="Autocommit Git-Cron: $c"
-			DISPLAY=:0 notify-send "Git-Cron Commits" "$(basename $1)"
 			git add .
-			git commit -m "$m"
+			git commit
 			git push
-			DISPLAY=:0 notify-send "Git-Cron Push" "$(basename $1) atualizado."
 			fi
 		fi
 	fi
