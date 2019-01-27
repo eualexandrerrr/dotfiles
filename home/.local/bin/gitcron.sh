a40e5547f735e22d19ea68519b03c6b945b5974b
mamutal91 <mamutal91@gmail.com>
2019-01-26 22:05:27 -0200

    gitcron: Add icon Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.local/bin/gitcron.sh
+++ home/.local/bin/gitcron.sh
@@ -20,7 +20,7 @@ atualiza() {
 			if [ ! -z "$status" ]; then
 			c=$(echo $(git add . -n | tr '\r\n' ' '))
 			m="Autocommit Git-Cron: $c"
-			DISPLAY=:0 notify-send "Git-Cron Commits" "$(basename $1)"
+			DISPLAY=:0 notify-send -i $icon "Git-Cron Commits" "$(basename $1)"
 			git add .
 			git commit -m "$m" -m "$committemp"
 			git push
