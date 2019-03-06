59e52b1b394ca85dac7673a04c144eaa05b67cf5
Alexandre Rangel <mamutal91@gmail.com>
2019-03-05 23:24:16 -0300

    Add zsh_history_backup
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

---
+++ home/.local/bin/zsh_history_backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# github.com/mamutal91
+
+cd /media/storage/Google\ Drive/Files/zsh_history
+rm -rf .zsh_history
+
+cd $HOME
+cp .zsh_history /media/storage/Google\ Drive/Files/zsh_history
