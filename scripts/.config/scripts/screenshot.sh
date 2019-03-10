6c2170356120b0f719245d402d081d6f3e099917
Alexandre Rangel <mamutal91@gmail.com>
2019-03-10 00:05:20 -0300

    scripts: Add time in screenshot
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- scripts/.config/scripts/screenshot.sh
+++ scripts/.config/scripts/screenshot.sh
@@ -18,6 +18,8 @@ file=$local/$name
 type="image/png"
 
 maim $var -u $file
-xclip -selection c -t $type -i $file
+echo $file
 DISPLAY=:0 dbus-launch notify-send -i $icon "Captura de tela" "$msg $name"
 canberra-gtk-play --file=$HOME/.config/files/sounds/screenshot.wav
+sleep 2;
+xclip -selection c -t $type -i $file
