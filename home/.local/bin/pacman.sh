18276384290336673ab1e5659a073aba46221bb6
Alexandre Rangel <mamutal91@gmail.com>
2019-03-10 18:37:21 -0300

    dotfiles: Fixes
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.local/bin/pacman.sh
+++ home/.local/bin/pacman.sh
@@ -6,24 +6,19 @@ icon=$iconsnotify/$app
 
 (cat ~/.cache/wal/sequences &)
 
-echo ▆▆ Atualizando Pacman
-sleep 1s
+echo "Atualizando Pacman"
     sudo pacman -Syyu --noconfirm
 
-echo ▆▆ Atualizando AUR
-sleep 1s
+echo "Atualizando AUR"
     yay -Syyu --noconfirm
 
-echo ▆▆ Removendo pacotes Pacman não utilizados
-sleep 1s
-    sudo pacman -Qdtq
-    sudo pacman -Rncs $(pacman -Qdtq)
+echo "Removendo pacotes Pacman não utilizados"
+    sudo pacman -Qdtq --noconfirm
+    sudo pacman -Rncs $(pacman -Qdtq) --noconfirm
 
-echo ▆▆ Removendo pacotes AUR não utilizados
-sleep 1s
-    yay -Qdtq
-    yay -Rncs $(yay -Qdtq)
+echo "Removendo pacotes AUR não utilizados"
+    yay -Qdtq --noconfirm
+    yay -Rncs $(yay -Qdtq) --noconfirm
 
 canberra-gtk-play --file=$HOME/.local/share/sounds/completed.wav
-
 DISPLAY=:0 dbus-launch notify-send -i $icon "Pacotes atualizados com sucesso."
