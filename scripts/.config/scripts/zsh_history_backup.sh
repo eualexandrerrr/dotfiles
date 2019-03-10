5a70cb10fffa6ea527e092f9ef54a2dcf7464251
Alexandre Rangel <mamutal91@gmail.com>
2019-03-10 00:06:26 -0300

    scripts: Remove zsh_history_backup
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- scripts/.config/scripts/zsh_history_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# github.com/mamutal91
-
-cd /media/storage/Google\ Drive/Files/zsh_history
-rm -rf .zsh_history
-
-cd $HOME
-cp -rf .zsh_history /media/storage/Google\ Drive/Files/zsh_history
