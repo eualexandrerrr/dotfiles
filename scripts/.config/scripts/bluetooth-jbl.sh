dd50c3bbfb845315051ac59f51c9e8b49d795a62
Alexandre Rangel <mamutal91@gmail.com>
2019-03-21 19:50:38 -0300

    bluetooth: Set icon JBL
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- scripts/.config/scripts/bluetooth-jbl.sh
+++ scripts/.config/scripts/bluetooth-jbl.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # github.com/mamutal91
 
+app=bluetooth-jbl.png
+icon=$iconsnotify/$app
+
 bluetoothctl power on
 bluetoothctl connect 78:44:05:BE:8A:7E
+
+
+DISPLAY=:0 dbus-launch notify-send -i $icon "BLUETOOTH" "Conectando headset JBL"
