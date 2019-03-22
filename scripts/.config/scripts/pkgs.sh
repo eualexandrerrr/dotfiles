f360b4fa9770ce3dd87c8cf8a599f2fe03620db0
Alexandre Rangel <mamutal91@gmail.com>
2019-03-22 13:34:15 -0300

    dotfiles: Welcome Franz
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- scripts/.config/scripts/pkgs.sh
+++ scripts/.config/scripts/pkgs.sh
@@ -22,8 +22,8 @@ readonly PKGS_PACMAN=(
     zsh zsh-syntax-highlighting)
 
 readonly PKGS_AUR=(
-    arc-gtk-theme capitaine-cursors grive-git gvfs-mtp nvidia-xrun nerd-fonts-complete papirus-icon-theme-git \
-    polybar smplayer-skins smplayer-themes spotify ttf-dejavu ttf-font-awesome whatsapp-nativefier xidlehook)
+    arc-gtk-theme capitaine-cursors franz grive-git gvfs-mtp nvidia-xrun nerd-fonts-complete papirus-icon-theme-git \
+    polybar smplayer-skins smplayer-themes spotify ttf-dejavu ttf-font-awesome xidlehook)
 
 function install_pkgs_pacman(){
     for i in "${PKGS_PACMAN[@]}"; do
