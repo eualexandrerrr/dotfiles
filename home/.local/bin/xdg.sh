0fa4c4b2ce262fb909c90c03435becff14c2bf7d
Alexandre Rangel <mamutal91@gmail.com>
2019-03-28 06:48:44 -0300

    xdg: Upload Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

---
+++ home/.local/bin/xdg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# github.com/mamutal91
+
+# Web
+xdg-mime default google-chrome.desktop 'x-scheme-handler/about'
+xdg-mime default google-chrome.desktop 'x-scheme-handler/htm'
+xdg-mime default google-chrome.desktop 'x-scheme-handler/html'
+xdg-mime default google-chrome.desktop 'x-scheme-handler/http'
+xdg-mime default google-chrome.desktop 'x-scheme-handler/https'
+
+# Images
+xdg-mime default gpicview.desktop 'x-scheme-handler/jpg'
+xdg-mime default gpicview.desktop 'x-scheme-handler/jpeg'
+xdg-mime default gpicview.desktop 'x-scheme-handler/gif'
+xdg-mime default gpicview.desktop 'x-scheme-handler/png'
+
+
+#application/vnd.oasis.opendocument.spreadsheet=libreoffice-calc.desktop;
+#image/png=feh.desktop;
+#video/x-matroska=vlc.desktop;smartcode-stremio.desktop;
+#application/x-ms-dos-executable=wine.desktop;
+#application/pdf=wine-extension-pdf.desktop;
+#image/gif=wine-extension-gif.desktop;
+#text/plain=wine-extension-txt.desktop;
+#text/html=google-chrome.desktop
