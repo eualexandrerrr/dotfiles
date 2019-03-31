ef4b8e6283435f1786b88534880369a8e7cb832d
Alexandre Rangel <mamutal91@gmail.com>
2019-03-31 17:52:55 -0300

    screenshot: New shortcuts
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.local/bin/screenshot.sh
+++ home/.local/bin/screenshot.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # github.com/mamutal91
 
+# -s = Recorte
+# -u = Fullscreen
+
 app=variety.png
 icon=$iconsnotify/$app
 
-variable=${1}
+var=${1}
+msg=${2}
+
 local="${HOME}/Imagens"
 name=$(date "+%d-%m-%Y_%H-%M-%S").png
 file=$local/$name
 type="image/png"
 
-[[ $variable == "-full" ]] && var="-u" || var="-s"
-[[ $variable == "-full" ]] && msg="Fullscreen" || msg="Recorte"
 [ ! -d $local ] && mkdir -p $local
 
 maim $var $file
-notify-send -i $icon "Captura de tela" "$msg. $name"
+notify-send -i $icon "Captura de tela" "$msg $name"
 canberra-gtk-play --file=$HOME/.local/share/sounds/screenshot.wav
 xclip -selection c -t $type -i $file
