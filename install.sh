2d1141cf5ff75c0ef7b24f3dfa0ffe6e472b5ea7
Alexandre Rangel <mamutal91@gmail.com>
2020-02-13 20:45:57 -0300

    install: Add aosp in first boot

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ function X11(){
 function first_boot() {
   echo "Removing default configs..."
   cd /home/mamutal91/.config
-  rm -rf alacritty compton dunst files gpicview i3 neofetch polybar rofi scripts smplayer
+  rm -rf alacritty aosp compton dunst files gpicview i3 neofetch polybar rofi scripts smplayer
   cd /etc/X11 && sudo rm -rf *
   cd /home/mamutal91/
   rm -rf .crontab .nanorc .nvidia-xinitrc .Xresources .zlogin .zshrc
