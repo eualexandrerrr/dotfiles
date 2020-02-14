3432bd1e12bc7582fde318d1fedb2b16259f273a
Alexandre Rangel <mamutal91@gmail.com>
2020-02-13 21:30:27 -0300

    scripts: Upload mount-storage

---
+++ scripts/.config/scripts/mount-storage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# github.com/mamutal91
+
+sudo dd if=/dev/urandom of=/root/keyfile bs=1024 count=4
+sudo chmod 0400 /root/keyfile
+sudo cryptsetup -v luksAddKey /dev/sdb1 /root/keyfile
+
+echo "sudo nano /etc/crypttab"
+echo "storage UUID=237c200d-3858-432c-b4cd-8e784bb8e605 /root/keyfile luks"
+echo
+echo "sudo nano /etc/fstab"
+echo "/dev/mapper/storage  /media/storage     ext4    defaults        0       2"
