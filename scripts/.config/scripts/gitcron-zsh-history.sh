539e4d4b7820d4502b0a7d37c8ca017e3ac61a84
Alexandre Rangel <mamutal91@gmail.com>
2020-02-13 23:31:34 -0300

    scripts: Add backup .zsh_history

---
+++ scripts/.config/scripts/gitcron-zsh-history.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# github.com/mamutal91
+
+dir="/media/storage/GitHub/"
+readonly repos=('zsh-history')
+
+app=git.png
+icon=$icons_path/$app
+
+function gitcron(){
+  for i in "${repos[@]}"; do
+			cd $dir/$i
+			status=$(git add . -n)
+			if [ ! -z "$status" ]; then
+				c=$(echo $(git add . -n | tr '\r\n' ' '))
+				m="Autocommit Git-Cron: $c"
+				git add .
+				git commit -m "$m" --author "Alexandre Rangel <mamutal91@gmail.com>" --date "$(date '+%Y-%m-%d %H:%M:%S')"
+				git push --force
+			fi
+done
+}
+
+gitcron
