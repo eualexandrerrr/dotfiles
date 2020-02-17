b44168d5dfcb85469f9b7d5356d293a77979a44e
Alexandre Rangel <mamutal91@gmail.com>
2020-02-16 22:21:31 -0300

    screenshot: Update script

--- scripts/.config/scripts/screenshot.sh
+++ scripts/.config/scripts/screenshot.sh
@@ -18,8 +18,8 @@ file=$local/$name
 type="image/png"
 
 maim $var -u $file
-sleep 2
-xclip -selection c -t $type -i $file
 sleep 1
+xclip -selection c -t $type -i $file
+
 DISPLAY=:0 dbus-launch notify-send -i $icon "Captura de tela" "$msg $name"
 canberra-gtk-play --file=$HOME/.config/files/sounds/screenshot.wav
