4d7a96c493ffbeac21227ccd57757dc93e8cab33
Alexandre Rangel <mamutal91@gmail.com>
2020-02-23 02:18:22 -0300

    pkgs: Add comment

--- scripts/.config/scripts/pkgs.sh
+++ scripts/.config/scripts/pkgs.sh
@@ -64,6 +64,7 @@ function install-yay() {
   rm -rf yay
 }
 
+# Dependencies for GTA V on SteamPlay (Proton)
 function winetricks() {
   winetricks --force directx9 vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 dotnet40 dotnet452 vb6 xact xna31 xna40 msl31 openal corefonts
 }
