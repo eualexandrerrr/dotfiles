951f6da49763ef7d1bff3db63e12fb5304a06312
Alexandre Rangel <mamutal91@gmail.com>
2020-03-07 13:26:16 -0300

    thunar/install: Upload Thunar configs and improvents install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # github.com/mamutal91
 
-echo "Initializing configurations..."
-echo "stow's..."
-echo "Removing actual .dotfiles and copying /media/storage/GitHub/dotfiles from /home/mamutal91/.dotfiles"
-
+# This replaces a folder at home with a folder that makes changes to my dotfiles
 rm -rf $HOME/.dotfiles && cp -rf /media/storage/GitHub/dotfiles $HOME/.dotfiles
 rm -rf $HOME/.zshrc
 cd $HOME/.dotfiles
@@ -21,40 +18,38 @@ for STOW in \
     polybar \
     rofi \
     scripts \
-    setup
+    setup \
+    thunar
 do
     stow $STOW
 done
 
 sudo stow bbswitch -t /etc
 
-function systemd() {
-  echo "systemd's..."
+function system() {
+  # SYSTEMD
   sudo rm -rf /etc/systemd/logind.conf
   sudo cp -rf $HOME/.dotfiles/systemd/logind.conf /etc/systemd
   sudo rm -rf /etc/systemd/system/getty@tty1.service.d/override.conf
   sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
   sudo cp -rf $HOME/.dotfiles/systemd/override.conf /etc/systemd/system/getty@tty1.service.d/
+
+  # X11
+  sudo rm -rf /etc/X11/nvidia-xorg.conf.d
+  sudo rm -rf /etc/X11/xorg.conf.d
+  sudo cp -rf $HOME/.dotfiles/X11/* /etc/X11
 }
 
-function X11() {
-    echo "X11..."
-    sudo rm -rf /etc/X11/nvidia-xorg.conf.d
-    sudo rm -rf /etc/X11/xorg.conf.d
-    sudo cp -rf $HOME/.dotfiles/X11/* /etc/X11
-}
-
-function first_boot() {
+function boot() {
   echo "Removing default configs..."
   cd $HOME/.config
-  rm -rf alacritty compton dunst files gpicview i3 neofetch polybar rofi scripts setup
+  rm -rf alacritty compton dunst files gpicview i3 neofetch polybar rofi scripts setup thunar
   rm -rf .crontab .nanorc .nvidia-xinitrc .Xresources .zlogin .zshrc
   cd /etc/X11 && sudo rm -rf *
 }
 
-#first_boot
-systemd
-X11
+#boot
+system
 
 canberra-gtk-play --file=$HOME/.config/files/sounds/completed.wav
 i3-msg restart
