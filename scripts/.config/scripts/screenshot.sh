29aaaba6a93855883616a5737677723f82d20615
Alexandre Rangel <mamutal91@gmail.com>
2020-03-09 18:53:09 -0300

    screenshot: Add time
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- scripts/.config/scripts/screenshot.sh
+++ scripts/.config/scripts/screenshot.sh
@@ -18,6 +18,7 @@ file=$local/$name
 type="image/png"
 
 maim $var -u $file
+sleep 2
 xclip -selection c -t $type -i $file
 sleep 1
 DISPLAY=:0 dbus-launch notify-send -i $icon "Captura de tela" "$msg $name"
