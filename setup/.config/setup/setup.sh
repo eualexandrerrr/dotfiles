b3a5494417c5597b3b50b2cfcd73e34bbe667598
Alexandre Rangel <mamutal91@gmail.com>
2020-03-17 20:14:09 -0300

    setup: Fix and improvents

--- setup/.config/setup/setup.sh
+++ setup/.config/setup/setup.sh
@@ -17,32 +17,37 @@ git clone https://aur.archlinux.org/yay.git $HOME/yay && cd "$HOME/yay" && makep
 # Load packages
 source $HOME/.dotfiles/setup/.config/setup/packages.sh
 
+
 # Install my packages
 for i in "${PACKAGES[@]}"; do
   sudo pacman -S ${i} --needed --noconfirm
 done
+
+# Spotify
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2EBF997C15BDA244B6EBF5D84773BD5E130D1D45
 
 for i in "${AUR[@]}"; do
   yay -S ${i} --needed --noconfirm
 done
 
 # Enable systemd services
-for SERVICE in \
+for SERVICES in \
     dhcpcd \
     NetworkManager \
     cronie \
     bumblebeed.service \
     bluetooth
 do
-    sudo systemctl enable $SERVICE
-    sudo systemctl start $SERVICE
+    sudo systemctl enable $SERVICES
+    sudo systemctl start $SERVICES
 done
 
 # Groups
-for GROUP in \
+for GROUPS in \
     bumblebeed
 do
-    sudo gpasswd -a mamutal91 $GROUP
+    sudo gpasswd -a mamutal91 $GROUPS
 done
 
 # Install oh-my-zsh
