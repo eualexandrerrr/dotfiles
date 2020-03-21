66ea329a477b00a5b5a09fccb08d4fd5c0ea0d0b
Alexandre Rangel <mamutal91@gmail.com>
2020-03-21 10:05:13 -0300

    polybar: Restore old model

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env sh
-
-## Add this to your wm startup file.
-
-# Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload up &
+    MONITOR=$m polybar --reload down &
+  done
+else
+  polybar --reload up &
+  polybar --reload down &
+fi
