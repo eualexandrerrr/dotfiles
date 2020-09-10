ae7591b0bef6dff616f549fafd76d42df50ba79a
Alexandre Rangel <mamutal91@gmail.com>
2020-09-10 05:05:55 -0300

    scripts/zsh-history: Fix path

--- aspire/.config/scripts/zsh-history.sh
+++ aspire/.config/scripts/zsh-history.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # github.com/mamutal91
 
-dir="/media/storage/GitHub/"
+dir="/media/storage/GITHUB/"
 readonly repos=('zsh-history')
 
-cp -rf $HOME/.zsh_history /media/storage/GitHub/zsh-history/
+cp -rf $HOME/.zsh_history /media/storage/GITHUB/zsh-history/
 
 function gitcron() {
   for i in "${repos[@]}"; do
