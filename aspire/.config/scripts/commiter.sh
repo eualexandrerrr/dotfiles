10644aa04aeb511931f18dfbd82a1a720d74073d
Alexandre Rangel <mamutal91@gmail.com>
2020-10-31 15:05:35 -0300

    scripts/commiter: add manifest repo

--- aspire/.config/scripts/commiter.sh
+++ aspire/.config/scripts/commiter.sh
@@ -6,10 +6,11 @@ dir=/media/storage/GITHUB
 repo0=" dotfiles"
 repo1=" mamutal91.github.io"
 repo2=" archlinux"
+repo3=" manifest"
 
-repos="$repo0\n$repo1\n$repo2"
+repos="$repo0\n$repo1\n$repo2\n$repo3"
 
-chosen="$(echo -e "$repos" | rofi -lines 3 -width 20% -dmenu -p "  Commiter")"
+chosen="$(echo -e "$repos" | rofi -lines 4 -width 20% -dmenu -p "  Commiter")"
 case $chosen in
     $repo0)
         alacritty -t newcommit --working-directory /home/mamutal91/.dotfiles;;
@@ -17,5 +18,7 @@ case $chosen in
         alacritty -t newcommit --working-directory $dir/mamutal91.github.io;;
     $repo2)
         alacritty -t newcommit --working-directory $dir/archlinux;;
+    $repo3)
+        alacritty -t newcommit --working-directory /media/storage/aosp-forking/manifest;;
 
 esac
