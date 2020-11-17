621041c0f3bcb04700d9b73ecbedcb4b5d670bd9
Alexandre Rangel <mamutal91@gmail.com>
2020-11-17 08:17:13 -0300

    exclude blur on maim

--- home/.config/scripts/screenshot.sh
+++ home/.config/scripts/screenshot.sh
@@ -8,6 +8,7 @@ icon="$HOME/.config/files/icons/screenshot.png"
 
 var=${1}
 msg=${2}
+geometry=$(slop)
 
 local=$HOME/Images/Screenshots
 [ ! -d $local ] && mkdir -p $local
@@ -16,7 +17,7 @@ name=$(date "+%d-%m-%Y_%H-%M-%S").png
 file=$local/$name
 type="image/png"
 
-maim $var -u $file
+maim $var -u $file $geometry
 sleep 1
 xclip -selection c -t $type -i $file
 
