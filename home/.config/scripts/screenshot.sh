219c9c3a3113571cdef2a6d6beb4ff8af48ee102
Alexandre Rangel <mamutal91@gmail.com>
2020-11-17 21:57:24 -0300

    scripts: screenshot updates by @sistematico

--- home/.config/scripts/screenshot.sh
+++ home/.config/scripts/screenshot.sh
@@ -4,22 +4,62 @@
 # -s = Recorte
 # -u = Fullscreen
 
-icon="$HOME/.config/files/icons/screenshot.png"
-
-var=${1}
-msg=${2}
-geometry=$(slop)
-
 local=$HOME/Images/Screenshots
 [ ! -d $local ] && mkdir -p $local
 
-name=$(date "+%d-%m-%Y_%H-%M-%S").png
-file=$local/$name
-type="image/png"
+dir="${HOME}/Images/Screenshots/"
 
-maim $var -u $file $geometry
-sleep 1
-xclip -selection c -t $type -i $file
+app="maim"
+#params="-u"
+params=""
+data=$(date +%Y-%m-%d_%H-%M-%S)
+nome="ss-${data}"
+extensao=".png"
+atraso=10
+icon="$HOME/.config/files/icons/screenshot.png"
+tipo="image/png"
+lixeira="${HOME}/.local/share/Trash"
+borda="10"
+
+[ ! -d $dir ] && mkdir -p $dir
+[ ! -d $lixeira ] && mkdir -p $lixeira
+
+if [ "$1" == "-c" ]; then
+	icone="${HOME}/.local/share/icons/elementary/user-trash.png"
+	listagem=(${dir}*)
+	if [ ${#listagem[@]} -gt 0 ]; then
+		mv ${dir}* ${lixeira}/files/
+		msg="Pasta de screenshots limpa!"
+		export DISPLAY=:0 ; canberra-gtk-play -i trash-empty 2>&1
+	else
+		msg="Pasta de screenshots já está vazia!"
+	fi
+	exit 0
+elif [ "$1" == "-w" ]; then
+	[ "$app" == "maim" ] && params="$params -i $(xdotool getactivewindow) -p $borda" || params="$params -w"
+	arquivo="${nome}-window${extensao}"
+	$app $params ${dir}${arquivo}
+elif [ "$1" == "-s" ]; then
+	params="$params -s"
+	arquivo="${nome}-sel${extensao}"
+	$app -d 2 $params ${dir}${arquivo}
+elif [ "$1" == "-d" ]; then
+	params="$params -d $atraso"
+	arquivo="${nome}-delay${extensao}"
+	$app $params ${dir}${arquivo}
+elif [ "$1" == "-e" ]; then
+	arquivo="${nome}-edit${extensao}"
+	$app $params ${dir}${arquivo}
+	viewnior ${dir}${arquivo}
+else
+	arquivo="${nome}${extensao}"
+	$app $params ${dir}${arquivo}
+fi
+
+if [ ! -z $arquivo ]; then
+	xclip -selection c -t $tipo -i $dir$arquivo
+fi
 
 notify-send -i $icon "Captura de tela" "$msg $name"
 canberra-gtk-play --file=$HOME/.config/files/sounds/screenshot.wav
+exit 0
