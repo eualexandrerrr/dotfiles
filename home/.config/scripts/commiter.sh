181916a37904226af2272bbeedd1ab2fdc0291cd
Alexandre Rangel <mamutal91@gmail.com>
2020-11-30 14:06:30 -0300

    Re-add aosp-build

--- home/.config/scripts/commiter.sh
+++ home/.config/scripts/commiter.sh
@@ -6,15 +6,16 @@ dir=/media/storage/GitHub
 repo0="dotfiles"
 repo1="mamutal91.github.io"
 repo2="android"
-repo3="archlinux-installer"
-repo4="readme (mamutal91)"
-repo5="device_xiaomi_beryllium"
-repo6="device_xiaomi_sdm845-common"
-repo7="AOSPK/manifest"
+repo3="aosp-build"
+repo4="archlinux-installer"
+repo5="readme (mamutal91)"
+repo6="device_xiaomi_beryllium"
+repo7="device_xiaomi_sdm845-common"
+repo8="AOSPK/manifest"
 
-repos="$repo0\n$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7"
+repos="$repo0\n$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7\n$repo8"
 
-chosen="$(echo -e "$repos" | rofi -lines 8 -width 20% -dmenu -p "  Commiter")"
+chosen="$(echo -e "$repos" | rofi -lines 9 -width 20% -dmenu -p "  Commiter")"
 case $chosen in
     $repo0)
         alacritty -t newcommit --working-directory /home/mamutal91/.dotfiles;;
@@ -23,14 +24,16 @@ case $chosen in
     $repo2)
         alacritty -t newcommit --working-directory $dir/android;;
     $repo3)
-        alacritty -t newcommit --working-directory $dir/archlinux-installer;;
+        alacritty -t newcommit --working-directory $dir/aosp-build;;
     $repo4)
-        alacritty -t newcommit --working-directory $dir/readme;;
+        alacritty -t newcommit --working-directory $dir/archlinux-installer;;
     $repo5)
-        alacritty -t newcommit --working-directory $dir/device_xiaomi_beryllium;;
+        alacritty -t newcommit --working-directory $dir/readme;;
     $repo6)
-        alacritty -t newcommit --working-directory $dir/device_xiaomi_sdm845-common;;
+        alacritty -t newcommit --working-directory $dir/device_xiaomi_beryllium;;
     $repo7)
+        alacritty -t newcommit --working-directory $dir/device_xiaomi_sdm845-common;;
+    $repo8)
         alacritty -t newcommit --working-directory /media/storage/AOSPK/manifest;;
 
 esac
