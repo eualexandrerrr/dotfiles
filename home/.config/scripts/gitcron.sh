12929c8ccb03831092ef70feda4257b247e63870
Alexandre Rangel <mamutal91@gmail.com>
2020-12-16 01:37:06 -0300

    Add scripts on gitcron

--- home/.config/scripts/gitcron.sh
+++ home/.config/scripts/gitcron.sh
@@ -2,7 +2,7 @@
 # github.com/mamutal91
 
 dir="/media/storage/GitHub/"
-readonly repos=('mamutal91.github.io' 'manual-custom-rom')
+readonly repos=('mamutal91.github.io' 'manual-custom-rom' 'scripts')
 
 function gitcron() {
   for i in "${repos[@]}"; do
