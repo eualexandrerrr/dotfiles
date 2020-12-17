28e5536a4f0108853fe94303b7c9fb74bb53fca6
Alexandre Rangel <mamutal91@gmail.com>
2020-12-17 18:33:13 -0300

    Improvents scripts and add color

--- home/.cmds.sh
+++ home/.cmds.sh
@@ -1,4 +1,24 @@
-# Functions
+#!/usr/bin/env bash
+
+# Colors
+END=$(tput sgr0)                    #  reset flag
+BLA=$END$(tput setaf 0)             #  black
+RED=$END$(tput setaf 1)             #  red
+GRE=$END$(tput setaf 2)             #  green
+YEL=$END$(tput setaf 3)             #  yellow
+BLU=$END$(tput setaf 4)             #  blue
+MAG=$END$(tput setaf 5)             #  magenta
+CYA=$END$(tput setaf 6)             #  cyan
+WHI=$END$(tput setaf 7)             #  white
+BOL=$(tput bold)                    ## bold flag
+BOL_BLA=$END$BOL$(tput setaf 0)     #  black
+BOL_RED=$END$BOL$(tput setaf 1)     #  red
+BOL_GRE=$END$BOL$(tput setaf 2)     #  green
+BOL_YEL=$END$BOL$(tput setaf 3)     #  yellow
+BOL_BLU=$END$BOL$(tput setaf 4)     #  blue
+BOL_MAG=$END$BOL$(tput setaf 5)     #  magenta
+BOL_CYA=$END$BOL$(tput setaf 6)     #  cyan
+BOL_WHI=$END$BOL$(tput setaf 7)     #  white
 
 function p () {
   git cherry-pick ${1}
@@ -32,29 +52,34 @@ function push () {
   then
     FORCE="&& git push -f"
   fi
-  echo "Pushing github.com/AOSPK/${1} - ${2}" && echo
+  echo "${BOL_GRE}Pushing github.com/AOSPK/${1} - ${2}${END}"
   git push ssh://git@github.com/AOSPK/${1} HEAD:refs/heads/${2} ${3}
 }
 
 function clone () {
-  echo "Cloning github.com/AOSPK/${1} - ${2}" && echo
+  echo "${BOL_BLU}Cloning github.com/AOSPK/${1} - ${2}${END}"
   git clone ssh://git@github.com/AOSPK/${1} -b ${2}
 }
 
 function los () {
-  echo "Cloning github.com/LineageOS/android_${1} - ${2}" && echo
+  echo "Cloning github.com/LineageOS/android_${1} - ${2}"
   rm -rf ${1} && git clone https://github.com/LineageOS/android_${1} -b ${2} ${1} && cd ${1}
 }
 
-function up() {
-  pwd_upstream=$(pwd)
-  REPO=${1}
-  LOS=${2}
-  AOSPK=${3}
-  cd $HOME && rm -rf $REPO
-  git clone https://github.com/LineageOS/android_$REPO -b $LOS $REPO
-  cd $REPO && git push ssh://git@github.com/AOSPK/$REPO HEAD:refs/heads/$AOSPK --force
-  cd $pwd_upstream
+function up () {
+  upstream ${1} lineage-18.1 eleven
+  upstream ${1} lineage-17.1 ten
+  upstream ${1} lineage-16.0 pie
+  upstream ${1} lineage-15.1 oreo-mr1
+  upstream ${1} cm-14.1 nougat
+}
+
+upstream () {
+  cd $HOME && rm -rf ${1}
+  echo "${BOL_CYA}Cloning LineageOS/android_${1} -b ${2}${END}"
+  git clone https://github.com/LineageOS/android_${1} -b ${2} ${1}
+  cd ${1} && push ${1} ${3}
+  rm -rf $HOME/${1}
 }
 
 # Tree for beryllium
@@ -73,7 +98,4 @@ function kernel () {
 function vendor () {
   rm -rf vendor/xiaomi
   git clone ssh://git@github.com/mamutal91/vendor_xiaomi -b eleven vendor/xiaomi
-}
-
-function www () {
 }
