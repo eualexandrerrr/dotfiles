0773e00372206520df762918d3f1844ba27fc8e8
Alexandre Rangel <mamutal91@gmail.com>
2020-12-24 18:35:41 -0300

    Updating Atom package repositories

--- home/.bin/scripts/atom.sh
+++ home/.bin/scripts/atom.sh
@@ -4,34 +4,14 @@
 # My favorites packages https://atom.io/packages/
 # Check the package name in the GitHub repository, in the file `package.json` for install
 
-pwd=$(pwd)
-
-rm -rf $HOME/.tmp && mkdir $HOME/.tmp && cd $HOME/.tmp
-
-for package in \
-    atom-material/atom-material-syntax \
-    Glavin001/atom-beautify \
-    Overload119/indent-sort \
-    abe33/atom-pigments \
-    denieler/save-workspace-atom-plugin \
-    file-icons/atom \
-    h3imdall/ftp-remote-edit \
-    lee-dohm/set-syntax \
-    taylon/language-i3wm \
-    thomaslindstrom/color-picker \
-    zhuochun/md-writer \
-    KunihikoKido/atom-flatten-json
-do
-    git clone https://github.com/$package
-done
-
-package='
+packages='
   atom-material-syntax
   atom-beautify
   color-picker
   file-icons
   ftp-remote-edit
   indent-sort
+  language-swaywm
   language-i3wm
   markdown-writer
   pigments
@@ -39,6 +19,4 @@ package='
   set-syntax
   flatten-json
   '
-apm install $package
-
-cd $pwd
+apm install $packages
