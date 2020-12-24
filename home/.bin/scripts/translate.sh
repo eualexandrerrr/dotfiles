4ac6fad727e16ab2df3a63c459a8cc90f763fc28
Alexandre Rangel <mamutal91@gmail.com>
2020-12-24 00:58:00 -0300

    /home/mamutal91/.tmp/.translate_result.txt

--- home/.bin/scripts/translate.sh
+++ home/.bin/scripts/translate.sh
@@ -8,5 +8,5 @@ rm -rf $typing $result
 nano $typing
 
 trans -b :en -i $typing > $result
-
-git add . && git commit --message $result --author "Alexandre Rangel <mamutal91@gmail.com>" && git push -f
+msg=$result
+git add . && git commit --message $msg --author "Alexandre Rangel <mamutal91@gmail.com>" && git push -f
