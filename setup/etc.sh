921579978d251bb4f5e38fbae7b09fc9eb23f393
Alexandre Rangel <mamutal91@gmail.com>
2020-12-25 00:05:46 -0300

    Revert "Remove script etc.sh as it will be handled by stow"
    This reverts commit 5d0ece61b5c68282bc468d59a82493a6b1b7b2f4.

---
+++ setup/etc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# github.com/mamutal91
+
+# systemd
+sudo rm -rf /etc/systemd/system/getty@tty1.service.d
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+sudo cp -rf $HOME/.dotfiles/etc/systemd/system/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d/
+
+# modprobe.d
+sudo rm -rf /etc/modprobe.d/ath10k_pci.conf
+sudo mkdir -p /etc/modprobe.d/
+sudo cp -rf $HOME/.dotfiles/etc/modprobe.d/ath10k_pci.conf /etc/modprobe.d/
