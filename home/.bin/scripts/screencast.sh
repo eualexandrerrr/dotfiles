98551ccb8fa205bae783c38be9a51bb1e232e165
Alexandre Rangel <mamutal91@gmail.com>
2020-12-24 21:57:22 -0300

    New script screencast (tmp) and fixes

--- home/.bin/scripts/screencast.sh
+++ home/.bin/scripts/screencast.sh
@@ -11,57 +11,10 @@
 # If there is already a running instance, user will be prompted to end it.
 
 dir="$HOME/Videos/Screencasts/"
+date=$(date +"%m%d%Y-%H%M")
 
-killrecording() {
-	recpid="$(cat /tmp/recordingpid)"
-	# kill with SIGTERM, allowing finishing touches.
-	kill -15 "$recpid"
-	rm -f /tmp/recordingpid
-	sleep 3
-	kill -9 "$recpid"
-	exit
-	}
-
-screencast() { \
-	wf-recorder -c libx264rgb -f "$dir/screencast-$(date '+%y%m%d-%H%M-%S').mkv" &
-	echo $! > /tmp/recordingpid
-}
-
-video() { \
-	wf-recorder -a -c libx264rgb -f "$dir/video-$(date '+%y%m%d-%H%M-%S').mkv" &
-	echo $! > /tmp/recordingpid
-	}
-
-window() { \
-	wf-recorder -a -c libx264rgb -g "$(slurp)" -f "$dir/window-$(date '+%y%m%d-%H%M-%S').mkv" &
-	echo $! > /tmp/recordingpid
-}
-
-vaapi() { \
-	wf-recorder -a -c h264_vaapi -d /dev/dri/renderD128 -f "$dir/video-$(date '+%y%m%d-%H%M-%S').mkv" &
-	echo $! > /tmp/recordingpid
-	}
-
-askrecording() { \
-  choice="$(printf "screencast\\nvideo\\nvaapi\\nwindow" | wofi --lines 9 --dmenu -p " ICONE Screencast")"
-#	choice=$(printf "screencast\\nvideo\\nvaapi\\nwindow" | wofi -p " 🎥 " -p "Select recording style:")
-	case "$choice" in
-		screencast) screencast;;
-		video) video;;
-		vaapi) vaapi;;
-		window) window;;
-	esac
-	}
-
-asktoend() { \
-	response=$(prompt "Recording still active. End recording?" "echo Yes") &&
-	[ "$response" = "Yes" ] &&  killrecording
-	}
-
-
-case "$1" in
-	screencast) screencast;;
-	video) video;;
-	kill) killrecording;;
-	*) ([ -f /tmp/recordingpid ] && asktoend && exit) || askrecording;;
-esac
+if [[ ${1} = "window" ]]; then
+	wf-recorder -g "$(slurp)" -f "$dir/window-$date.mp4"
+else
+	wf-recorder -f "$dir/full-$date.mp4"
+fi
