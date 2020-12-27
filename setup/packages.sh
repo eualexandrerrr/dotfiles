46435ed63a1602c8e50ce4d725561df78bd35d71
Alexandre Rangel <mamutal91@gmail.com>
2020-12-27 02:56:20 -0300

    IWantToSpeak is dead

--- setup/packages.sh
+++ setup/packages.sh
@@ -12,7 +12,7 @@ readonly PACKAGES=(
   # apps
   alacritty atom
   cronie
-  firefox firefox-i18n-pt-br fortune-mod
+  firefox firefox-i18n-pt-br
   git gpicview gimp gimp-help-pt_br
   htop
   lxappearance
