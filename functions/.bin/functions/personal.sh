4dd4acad5f2278d42924f0fa16977e7e8bd3c455
Alexandre Rangel <mamutal91@gmail.com>
2020-12-26 23:33:38 -0300

    Putting the SSH generator to work saving scripts

--- functions/.bin/functions/personal.sh
+++ functions/.bin/functions/personal.sh
@@ -3,3 +3,10 @@
 function blog () {
   $HOME/.bin/scripts/blog.sh
 }
+
+function sshgithub() {
+  ssh-keygen -t ed25519 -C "mamutal91@github.com"
+  eval "$(ssh-agent -s)" && ssh-add -l
+  cat $HOME/.ssh/id_rsa.pub | wl-copy
+  xdg-open https://github.com/settings/ssh/new
+}
--- scripts/.bin/scripts/generate-ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# github.com/mamutal91
-
-if [[ $USER = "mamutal91" ]]; then
-  ssh-keygen -t rsa -b 4096 -C "mamutal91@gmail.com"
-  eval "$(ssh-agent -s)" && ssh-add -l
-  cat $HOME/.ssh/id_rsa.pub | wl-copy
-  xdg-open https://github.com/settings/ssh/new
-fi
