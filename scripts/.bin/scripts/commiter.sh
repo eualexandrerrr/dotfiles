4c7ce10b4899d14bf2c7f36e287c97e47f852af4
Alexandre Rangel <mamutal91@gmail.com>
2020-12-27 00:07:02 -0300

    Organization improvements in the committer

--- scripts/.bin/scripts/commiter.sh
+++ scripts/.bin/scripts/commiter.sh
@@ -7,37 +7,37 @@
 github=/media/storage/GitHub
 aospk=/media/storage/AOSPK
 
-repo0="dotfiles"
-repo1="mamutal91.github.io"
-repo2="myarch"
-repo3="buildbot"
-repo4="readme"
-repo5="device_xiaomi_beryllium"
-repo6="device_xiaomi_sdm845-common"
-repo7="AOSPK/manifest"
-repo8="AOSPK/docs"
+repo1="dotfiles"
+repo2="mamutal91.github.io"
+repo3="myarch"
+repo4="buildbot"
+repo5="readme"
+repo6="device_xiaomi_beryllium"
+repo7="device_xiaomi_sdm845-common"
+repo8="AOSPK/manifest"
+repo9="AOSPK/docs"
 
-repos="$repo0\n$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7\n$repo8"
+repos="$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7\n$repo8\n$repo9"
 
-chosen="$(echo -e "$repos" | wofi --lines 9 --dmenu -p "  Commiter")"
+chosen="$(echo -e "$repos" | wofi --lines 9 --sort-order=DEFAULT --dmenu -p "  Commiter")"
 case $chosen in
-    $repo0)
-        alacritty -t newcommit --working-directory /home/mamutal91/.dotfiles;;
     $repo1)
-        alacritty -t newcommit --working-directory $github/mamutal91.github.io;;
+        alacritty -t newcommit --working-directory /home/mamutal91/.dotfiles;;
     $repo2)
-        alacritty -t newcommit --working-directory $github/myarch;;
+        alacritty -t newcommit --working-directory $github/mamutal91.github.io;;
     $repo3)
-        alacritty -t newcommit --working-directory $github/buildbot;;
+        alacritty -t newcommit --working-directory $github/myarch;;
     $repo4)
-        alacritty -t newcommit --working-directory $github/readme;;
+        alacritty -t newcommit --working-directory $github/buildbot;;
     $repo5)
-        alacritty -t newcommit --working-directory $github/device_xiaomi_beryllium;;
+        alacritty -t newcommit --working-directory $github/readme;;
     $repo6)
-        alacritty -t newcommit --working-directory $github/device_xiaomi_sdm845-common;;
+        alacritty -t newcommit --working-directory $github/device_xiaomi_beryllium;;
     $repo7)
-        alacritty -t newcommit --working-directory $aospk/manifest;;
+        alacritty -t newcommit --working-directory $github/device_xiaomi_sdm845-common;;
     $repo8)
+        alacritty -t newcommit --working-directory $aospk/manifest;;
+    $repo9)
         alacritty -t newcommit --working-directory $aospk/docs;;
 esac
 exit 0;
