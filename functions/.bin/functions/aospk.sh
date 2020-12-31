877bee14d44e71828adea5d28f6863e0524107df
Alexandre Rangel <mamutal91@gmail.com>
2020-12-31 18:28:54 -0300

    Adding functions to compile in the terminal

--- functions/.bin/functions/aospk.sh
+++ functions/.bin/functions/aospk.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+function s() {
+  repo init -u ssh://git@github.com/AOSPK/manifest -b eleven
+  repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --force-sync
+}
+
+function b() {
+  mkdir /home/ccache/AOSPK &>/dev/null
+  export CCACHE_EXEC=$(which ccache)
+  export USE_CCACHE=1
+  export CCACHE_DIR=/home/ccache/AOSPK
+  ccache -M 300G &>/dev/null
+  . build/envsetup.sh
+  lunch aosp_beryllium-userdebug
+  make -j$(nproc --all) bacon 2>&1 | tee log.txt
+}
+
+function bp() {
+  s && b
+}
+
 function tree () {
   rm -rf device/xiaomi/beryllium device/xiaomi/sdm845-common hardware/xiaomi
   git clone ssh://git@github.com/mamutal91/device_xiaomi_beryllium -b eleven device/xiaomi/beryllium
