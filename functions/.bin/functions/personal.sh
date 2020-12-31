fd12610a051c63640c993772f56d8081449c9774
Alexandre Rangel <mamutal91@gmail.com>
2020-12-31 17:32:40 -0300

    Correcting SSH key name for GitHub

--- functions/.bin/functions/personal.sh
+++ functions/.bin/functions/personal.sh
@@ -7,6 +7,6 @@ function blog () {
 function sshgithub() {
   ssh-keygen -t ed25519 -C "mamutal91@github.com"
   eval "$(ssh-agent -s)" && ssh-add -l
-  cat $HOME/.ssh/id_rsa.pub | wl-copy
+  cat $HOME/.ssh/id_ed25519.pub | wl-copy
   xdg-open https://github.com/settings/ssh/new
 }
