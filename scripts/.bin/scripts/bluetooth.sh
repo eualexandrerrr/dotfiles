0a80778baf7a936db32eb32526aa559001f9b9b4
Alexandre Rangel <mamutal91@gmail.com>
2021-01-01 00:27:06 -0300

    Adding notification due to bluetooth

--- scripts/.bin/scripts/bluetooth.sh
+++ scripts/.bin/scripts/bluetooth.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+icon="${iconpath}/bluetooth.svg"
+
 bluetoothctl power on
 bluetoothctl agent on
 bluetoothctl default-agent
@@ -19,7 +21,7 @@ devices="$scan\n$bt1\n$bt2"
 chosen="$(echo -e "$devices" | wofi --lines 3 --sort-order=DEFAULT --dmenu -p "  Bluetooth")"
 case $chosen in
     $scan)
-      bluetoothctl scan on &>/dev/null;;
+      bluetoothctl scan on && notify-send -i $icon "Bluetooth" "A lista de dispositivos disponíveis foi atualizada!";;
     $bt1)
       bt "F1:32:33:23:43:4C";;
     $bt2)
