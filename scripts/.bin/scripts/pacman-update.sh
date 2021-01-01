573ec42cd0deb159318f4a2926dc7dabdda6e24d
Alexandre Rangel <mamutal91@gmail.com>
2021-01-01 18:41:33 -0300

    Translating pacman updater to English

--- scripts/.bin/scripts/pacman-update.sh
+++ scripts/.bin/scripts/pacman-update.sh
@@ -4,15 +4,15 @@ icon="${iconpath}/modem.svg"
 
 sleep 1
 
-echo "Atualizando Pacman e AUR"
+echo "Updating Pacman and AUR"
   sudo pacman -Syu
   yay -Syu
 
-echo "Removendo pacotes não utilizados"
+echo "Removing unused packages"
   sudo pacman -Qdtq --noconfirm
   yay -Qdtq --noconfirm
   sudo pacman -Rncs $(pacman -Qdtq) --noconfirm
   yay -Rncs $(yay -Qdtq) --noconfirm
 
 play $HOME/.bin/sounds/completed.wav
-notify-send -i $icon "archlinux" "Pacotes atualizados com sucesso."
+notify-send -i $icon "archlinux" "Successfully updated packages."
