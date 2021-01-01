58abf8d3572859800c539b11cacb9f94f920db82
Alexandre Rangel <mamutal91@gmail.com>
2021-01-01 19:07:24 -0300

    Add colors in install

--- install.sh
+++ install.sh
@@ -2,20 +2,21 @@
 
 clear
 
+source $HOME/.colors
 rm -rf $HOME/.bash_profile .bashrc
 
 cd $HOME/.dotfiles
 for DOTFILES in $(find . -maxdepth 1  -not -name "etc" ! -name ".*" ! -name "setup" -type d -printf '%f\n')
 do
   stow --adopt $DOTFILES || echo "Error on gnu/stow"
-  echo "$DOTFILES stowed."
+  echo "${RED}$DOTFILES ${GRE}stowed.${END}"
 done
 
 # etc
 if [ "$HOSTNAME" = "odin" ];
 then
   $HOME/.dotfiles/setup/etc.sh || echo "Error configure etc"
-  echo "/etc/ configured."
+  echo "${RED}/etc/ ${GRE}configured.${END}"
 fi
 
 play $HOME/.local/share/sounds/completed.wav
