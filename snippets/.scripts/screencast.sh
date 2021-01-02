69fe9b821bc713e265fb692404c408e78560c339
Alexandre Rangel <mamutal91@gmail.com>
2021-01-02 16:42:29 -0300

    Add new screen recording script and correcting screenshot

--- snippets/.scripts/screencast.sh
+++ snippets/.scripts/screencast.sh
@@ -7,8 +7,21 @@ fi
 
 date=$(date +"%m%d%Y-%H%M")
 
-if [[ ${1} = "window" ]]; then
-	wf-recorder -g "$(slurp)" -f "$dir/window-$date.mp4"
-else
-	wf-recorder -f "$dir/full-$date.mp4"
-fi
+icon="${iconpath}/bluetooth.svg"
+
+window=" Window"
+full=" Full"
+kill=" Stop recorder"
+
+devices="$window\n$full\n$kill"
+
+chosen="$(echo -e "$devices" | wofi --lines 3 --sort-order=DEFAULT --dmenu -p "  Screencast")"
+case $chosen in
+    $window)
+			wf-recorder -g "$(slurp)" -f "$dir/window-$date.mp4";;
+    $full)
+      wf-recorder -f "$dir/full-$date.mp4";;
+    $kill)
+      pkill -2 wf-recorder;;
+esac
+exit 0;
