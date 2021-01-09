6ee44a528b8ec041b05c6f4c8bb63df1bc2017b8
Alexandre Rangel <mamutal91@gmail.com>
2021-01-09 18:10:58 -0300

    Generate log in builds

--- snippets/.functions/aospk.sh
+++ snippets/.functions/aospk.sh
@@ -5,7 +5,7 @@ function s() {
 }
 
 function b() {
-  . build/envsetup.sh & lunch aosp_beyllium-userdebug && make bacon -j$(nproc --all)
+  . build/envsetup.sh & lunch aosp_beyllium-userdebug && make bacon -j$(nproc --all) | tee log.txt
 }
 
 function bp() {
