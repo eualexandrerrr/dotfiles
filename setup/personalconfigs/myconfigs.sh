bafe596d6eb1749f6d023f350f73c482fb3fa2e9
Alexandre Rangel <mamutal91@aospk.org>
2021-03-03 14:13:24 -0300

    Fix keys
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/personalconfigs/myconfigs.sh
+++ setup/personalconfigs/myconfigs.sh
@@ -30,8 +30,8 @@ pwd=$(pwd)
 cp -rf $HOME/GitHub/mytokens $HOME && rm -rf $HOME/.mytokens && mv $HOME/mytokens $HOME/.mytokens
 cd $HOME/GitHub/mytokens/.ssh
 mkdir -p $HOME/.ssh
-cp -rf id_rsa* $HOME/.ssh
-sudo chmod 600 $HOME/.ssh/id_rsa*
+cp -rf * $HOME/.ssh
+sudo chmod 600 $HOME/.ssh/*
 cd $pwd
 
 # Clone my important repos
