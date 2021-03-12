e6658613de9a18ce4c2739d72fc92d37a6f54017
Alexandre Rangel <mamutal91@gmail.com>
2021-03-12 17:20:01 -0300

    Modifications to the function of the lmi tree
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/kraken/functions.sh
+++ home/.config/kraken/functions.sh
@@ -3,12 +3,13 @@
 function treeseba() {
   pdw=$(pwd)
   cd $HOME
-  rm -rf android_device_xiaomi_lmi android_device_xiaomi_lmi-kernel android_device_xiaomi_sm8250-common
+  rm -rf android_device_xiaomi_lmi android_device_xiaomi_lmi-kernel android_device_xiaomi_sm8250-common android_kernel_xiaomi_sm8250
   rm -rf android_vendor_xiaomi vendor_xiaomi_lmi vendor_xiaomi_sm8250-common
 
   git clone https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_lmi -b lineage-18.1
   git clone https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_lmi-kernel -b lineage-18.1
   git clone https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_sm8250-common -b lineage-18.1
+  git clone https://github.com/xiaomi-sm8250-devs/android_kernel_xiaomi_sm8250 -b lineage-18.1
 
   cd $HOME/android_device_xiaomi_lmi
   git push ssh://git@github.com/AOSPK-Devices/device_xiaomi_lmi HEAD:refs/heads/eleven --force
@@ -19,6 +20,10 @@ function treeseba() {
   cd $HOME/android_device_xiaomi_sm8250-common
   git push ssh://git@github.com/AOSPK-Devices/device_xiaomi_sm8250-common HEAD:refs/heads/eleven --force
 
+  cd $HOME/android_kernel_xiaomi_sm8250
+  git push ssh://git@github.com/AOSPK-Devices/kernel_xiaomi_sm8250 HEAD:refs/heads/eleven --force
+
+  cd $HOME
   git clone https://git.rip/xiaomi-sm8250-devs/android_vendor_xiaomi -b lineage-18.1
   mv android_vendor_xiaomi vendor_xiaomi_lmi
   cp -rf vendor_xiaomi_lmi vendor_xiaomi_sm8250-common
