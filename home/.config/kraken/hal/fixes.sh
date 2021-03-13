dc035b3d8d352b6c22b45cca9719e42f965a09ff
Alexandre Rangel <mamutal91@gmail.com>
2021-03-13 00:57:49 -0300

    No longer need the hal fix for GCAM on the lmi
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/kraken/hal/fixes.sh
+++ home/.config/kraken/hal/fixes.sh
@@ -3,13 +3,6 @@
 # FIXES
 cd /tmp/
 rm -rf hardware_qcom_display
-git clone ssh://git@github.com/AOSPK/hardware_qcom_display -b eleven-caf-sm8250
-cd hardware_qcom_display
-git fetch "https://github.com/LineageOS/android_hardware_qcom_display" refs/changes/18/305418/1 && git cherry-pick FETCH_HEAD
-git push
-
-cd /tmp/
-rm -rf hardware_qcom_display
 git clone ssh://git@github.com/AOSPK/hardware_qcom_display -b eleven-caf-sm8150
 cd hardware_qcom_display
 git fetch https://github.com/PixelExperience/hardware_qcom-caf_sm8150_display eleven && git cherry-pick 4133e8b2f42dd06935c1560254ca96336e41512c
