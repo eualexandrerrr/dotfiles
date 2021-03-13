cc021f0cc3660fb7a5563632b99f245680017f88
Alexandre Rangel <mamutal91@aospk.org>
2021-03-12 21:00:28 -0300

    Important updates
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/packages.sh
+++ setup/packages.sh
@@ -5,10 +5,10 @@ dependencies=(
   pulseaudio alsa-firmware alsa-utils alsa-plugins pulseaudio pulseaudio-bluetooth pavucontrol sox
   bluez bluez-libs bluez-tools bluez-utils
 
-  i3-gaps i3lock feh rofi dunst picom polybar alacritty stow nano nano-syntax-highlighting neofetch vlc gpicview zsh zsh-syntax-highlighting oh-my-zsh-git maim ffmpeg imagemagick slop
+  i3-gaps i3lock feh rofi dunst picom polybar-git alacritty stow nano nano-syntax-highlighting neofetch vlc gpicview zsh zsh-syntax-highlighting oh-my-zsh-git maim ffmpeg imagemagick slop
   thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman
 
-  flatery-icon-theme-git vimix-cursors dracula-gtk-theme
+  flatery-icon-theme-git vimix-cursors-git dracula-gtk-theme-git
 
   google-chrome kotatogram-desktop-bin
 
@@ -25,7 +25,7 @@ mypackages=(
   mesa mesa-demos vulkan-tools lib32-nvidia-utils lib32-opencl-nvidia lib32-virtualgl lib32-nvidia-utils lib32-libvdpau lib32-opencl-nvidia lib32-mesa
   steam wine winetricks lib32-gnutls
 
-  atom discord diff-so-fancy filezilla git htop jdk-openjdk jq krita man man-pages-pt_br github-cli-git gitlab-glab-bin
+  atom discord diff-so-fancy filezilla git htop jdk-openjdk jq krita man man-pages-pt_br github-cli-git
   rsync shfmt tree transmission-gtk zip translate-shell-git
 
   android-tools hfsprogs gvfs gvfs-mtp btrfs-progs dosfstools exfat-utils f2fs-tools e2fsprogs jfsutils nilfs-utils ntfs-3g reiserfsprogs udftools xfsprogs ntfs-3g
