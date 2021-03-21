42b0997477c1a7418a722dace5c7ec74d23ad72f
Alexandre Rangel <mamutal91@gmail.com>
2021-03-21 08:58:33 -0300

    Move Kraken scripts to the Utils folder
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/kraken/utils/bookmarks.sh
+++ home/.config/kraken/utils/bookmarks.sh
@@ -17,7 +17,7 @@ sed -i "s/custom/lineage/g" $tmp
 sed -i "s/Custom/Lineage/g" $tmp
 sed -i "s/aosp/lineage/g" $tmp
 sed -i "s/Launcher3/Trebuchet/g" $tmp
-sed -i "s/packages_apps_LineageLineageizer/packages_apps_LineageCustomizer/g" $tmp
+sed -i "s/packages_apps_LineageLineageizer/packages_apps_TvCustomizer/g" $tmp
 
 cat $tmp > $HOME/bookmarks_names.txt
 
