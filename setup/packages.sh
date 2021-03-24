7418cf8b324b269d462ba6153c680a75fbd2b8be
Alexandre Rangel <mamutal91@gmail.com>
2021-03-24 16:01:16 -0300

    Add gnupg
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/packages.sh
+++ setup/packages.sh
@@ -21,7 +21,7 @@ dependencies=(
 
 # My perosnal packages!!!
 mypackages=(
-  archlinux-keyring cronie
+  archlinux-keyring gnupg cronie
 
   nvidia nvidia-utils nvidia-settings nvidia-utils nvidia-dkms nvidia-prime opencl-nvidia nbfc-git
   mesa mesa-demos vulkan-tools lib32-nvidia-utils lib32-opencl-nvidia lib32-virtualgl lib32-nvidia-utils lib32-libvdpau lib32-opencl-nvidia lib32-mesa
