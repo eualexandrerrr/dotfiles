2870ce39007b66f55bd15cdfc48dc5e60246f58c
Alexandre Rangel <mamutal91@gmail.com>
2021-03-27 02:57:19 -0300

    neck limp
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

---
+++ allscripts/.config/scripts/kraken/hal/limp.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source $HOME/.Xcolors &> /dev/null
+
+limps=(
+  "vendor_nxp_opensource_halimpl twelve-pn5xx"
+  "vendor_nxp_opensource_hidlimpl twelve-pn5xx"
+  "vendor_nxp_opensource_halimpl twelve-sn100x"
+  "vendor_nxp_opensource_hidlimpl twelve-sn100x"
+)
+
+for i in "${limps[@]}"; do
+  workingDir=$(mktemp -d) && cd $workingDir
+
+  repo=$(echo ${i} | awk '{ print $1 }')
+  branch=$(echo ${i} | awk '{ print $2 }')
+  branchBase=$(sed "s:twelve:arrow-12.0:g" <<< $branch)
+
+  echo -e "\n${BOL_GRE}Repo   : ${repo}${END}"
+  echo -e "${BOL_GRE}Branch : ${branch}${END}"
+  echo -e "${BOL_GRE}Base   : ${branchBase}\n${END}"
+
+  git clone https://github.com/ArrowOS/android_${repo} -b ${branchBase} ${repo}
+  cd ${repo}
+  git push ssh://git@github.com/AOSPK/${repo} HEAD:refs/heads/${branch} --force
+  git push ssh://git@github.com/AOSPK-Next/${repo} HEAD:refs/heads/${branch} --force
+
+  cd $workingDir
+done
