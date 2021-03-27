1ad118a48dbb49960315ff71bbbc32d31550820a
Alexandre Rangel <mamutal91@gmail.com>
2021-03-27 08:52:33 -0300

    Giving necessary permissions to the systemd autologin
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/etc.sh
+++ setup/etc.sh
@@ -6,6 +6,8 @@ if [[ $HOST = "odin" ]]; then
 fi
 
 # systemd Auto Login
+sudo rm -rf /etc/systemd/system/getty@tty1.service.d/override.conf
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
 echo "[Service]
 ExecStart=
 ExecStart=-/usr/bin/agetty --autologin $USER --noclear %I $TERM" | sudo tee -a /etc/systemd/system/getty@tty1.service.d/override.conf > /dev/null
