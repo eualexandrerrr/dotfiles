7170290d4f36ad7eb1d50da08152f6d007747bbc
Alexandre Rangel <mamutal91@gmail.com>
2021-03-28 15:23:54 -0300

    Run script lmi

--- home/.config/kraken/functions.sh
+++ home/.config/kraken/functions.sh
@@ -1,50 +1,7 @@
 #!/usr/bin/env bash
 
 function lmi() {
-  pdw=$(pwd)
-
-  # TREE
-  cd $HOME
-  rm -rf android_device_xiaomi_lmi android_device_xiaomi_lmi-kernel android_device_xiaomi_sm8250-common android_kernel_xiaomi_sm8250
-  rm -rf android_vendor_xiaomi vendor_xiaomi_lmi vendor_xiaomi_sm8250-common
-
-  if [[ ${1} = tree ]]; then
-    cd $HOME
-    git clone https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_lmi -b lineage-18.1
-    git clone https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_sm8250-common -b lineage-18.1
-    git clone https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_lmi-kernel -b lineage-18.1
-
-    cd $HOME/android_device_xiaomi_lmi
-    git push ssh://git@github.com/AOSPK-Devices/device_xiaomi_lmi HEAD:refs/heads/eleven --force
-
-    cd $HOME/android_device_xiaomi_sm8250-common
-    git push ssh://git@github.com/AOSPK-Devices/device_xiaomi_sm8250-common HEAD:refs/heads/eleven --force
-
-    cd $HOME/android_device_xiaomi_lmi-kernel
-    git push ssh://git@github.com/AOSPK-Devices/device_xiaomi_lmi-kernel HEAD:refs/heads/eleven --force
-  fi
-
-  # VENDOR
-  cd $HOME
-  git clone https://gitlab.com/xiaomi-sm8250-devs/android_vendor_xiaomi -b lineage-18.1
-  mv android_vendor_xiaomi vendor_xiaomi_lmi
-  cp -rf vendor_xiaomi_lmi vendor_xiaomi_sm8250-common
-
-  cd $HOME/vendor_xiaomi_lmi
-  git filter-branch --prune-empty --subdirectory-filter lmi lineage-18.1
-  git push ssh://git@gitlab.com/AOSPK-Devices/vendor_xiaomi_lmi HEAD:refs/heads/eleven --force
-
-  cd $HOME/vendor_xiaomi_sm8250-common
-  git filter-branch --prune-empty --subdirectory-filter sm8250-common lineage-18.1
-  git push ssh://git@gitlab.com/AOSPK-Devices/vendor_xiaomi_sm8250-common HEAD:refs/heads/eleven --force
-
-  # KERNEL
-  cd $HOME
-  git clone https://github.com/xiaomi-sm8250-devs/android_kernel_xiaomi_sm8250 -b lineage-18.1
-
-  cd $HOME/android_kernel_xiaomi_sm8250
-  git push ssh://git@github.com/AOSPK-Devices/kernel_xiaomi_sm8250 HEAD:refs/heads/eleven --force
-
+  cd $HOME && $HOME/.dotfiles/home/.config/kraken/lmi.sh
 }
 
 function beryllium() {
