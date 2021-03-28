947bcc8bf6d39a2539b0ac8aa95f73647887dcaf
Alexandre Rangel <mamutal91@gmail.com>
2021-03-28 17:13:49 -0300

    &>/dev/null for update.sh
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/scripts/pacman-update.sh
+++ home/.config/scripts/pacman-update.sh
@@ -15,5 +15,5 @@ echo "${GRE}Removing unused packages${END}"
   sudo pacman -Rncs $(pacman -Qdtq) --noconfirm
   yay -Rncs $(yay -Qdtq) --noconfirm
 
-play $HOME/.config/sounds/completed.wav
+play $HOME/.config/sounds/completed.wav &>/dev/null
 notify-send -i $icon "ArchLinux" "Successfully updated packages."
