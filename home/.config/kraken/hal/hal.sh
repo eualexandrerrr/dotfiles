8746658a0bcd3e3d164ab71453ef35bb4090fb83
Alexandre Rangel <mamutal91@gmail.com>
2021-03-28 21:41:49 -0300

    Introducing fix for hal, after updates
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/kraken/hal/hal.sh
+++ home/.config/kraken/hal/hal.sh
@@ -20,4 +20,13 @@ for i in "${REPOS_HAL[@]}"; do
   git push ssh://git@github.com/AOSPK/${i} HEAD:refs/heads/$AOSPK_HAL --force
 done
 
+# FIXES
+cd /tmp/
+
+rm -rf hardware_qcom_display
+git clone ssh://git@github.com/AOSPK/hardware_qcom_display -b eleven-caf-sm8250
+cd hardware_qcom_display
+git fetch "https://github.com/LineageOS/android_hardware_qcom_display" refs/changes/18/305418/1 && git cherry-pick FETCH_HEAD
+git push
+
 rm -rf $tmp
