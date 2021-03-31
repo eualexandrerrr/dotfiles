2858ae66b227cd1921fd14130af0d34d0a78b39e
Alexandre Rangel <mamutal91@gmail.com>
2021-03-31 00:48:29 -0300

    Improvements in gitcron
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/scripts/gitcron.sh
+++ home/.config/scripts/gitcron.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
-dir="$HOME/"
+dir="$HOME/GitHub"
 cp -rf $HOME/.zsh_history $HOME/GitHub/zsh-history/
 
-readonly repos=('GitHub/zsh-history' 'GitHub/custom-rom' 'AOSPK/design' '/mnt/roms/sites/docker/gerrit')
+readonly repos=(
+  'zsh-history'
+  'custom-rom'
+  'gerrit'
+  'jenkins'
+  'userge-x'
+)
 
-function gitcron() {
-  for i in "${repos[@]}"; do
-			cd $dir/$i
-			status=$(git add . -n)
-			if [[ ! -z "$status" ]]; then
-				c=$(echo $(git add . -n | tr '\r\n' ' '))
-				m="Autocommit Git-Cron: $c"
-				git add .
-				git commit -m "$m" --author "Alexandre Rangel <mamutal91@gmail.com>" --date "$(date '+%Y-%m-%d %H:%M:%S')"
-				git push
-			fi
+for i in "${repos[@]}"; do
+  if [[ ${i} = gerrit || ${i} = jenkins || ${i} = userge-x ]]; then
+    dir="/mnt/roms/sites/docker"
+  fi
+	cd $dir/$i
+	status=$(git add . -n)
+	if [[ ! -z "$status" ]]; then
+		c=$(echo $(git add . -n | tr '\r\n' ' '))
+		m="Autocommit Git-Cron: $c"
+		git add .
+		git commit -m "$m" --author "Alexandre Rangel <mamutal91@gmail.com>" --date "$(date '+%Y-%m-%d %H:%M:%S')"
+		git push
+	fi
 done
-}
-
-gitcron
