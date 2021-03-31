01e3f8705a6a673a7fcfa35664b7deebe1bed3ac
Alexandre Rangel <mamutal91@gmail.com>
2021-03-31 01:18:32 -0300

    Fix repositories
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/scripts/repositories.sh
+++ home/.config/scripts/repositories.sh
@@ -3,29 +3,26 @@
 rm -rf $HOME/AOSPK && mkdir -p $HOME/AOSPK
 rm -rf $HOME/GitHub && mkdir -p $HOME/GitHub
 
-ACCOUNT=mamutal91
-FOLDER=GitHub
-
-readonly mamutal91=(
-    mamutal91.github.io
-    myarch
-    infra
-    custom-rom
-    zsh-history
-    language-swaywm
-    shellscript-atom-snippets
-    manifest
-    docs
-    infra
+mamutal91=(
+  mamutal91.github.io
+  myarch
+  custom-rom
+  zsh-history
+  language-swaywm
+  shellscript-atom-snippets
+  infra
+  manifest
+  docs
 )
 
 for i in "${mamutal91[@]}"; do
-  if [[ ${i} = manifest || ${i} = docs || ${i} = infra ]]; then
-    ACCOUNT=AOSPK
-    FOLDER=AOSPK
+  account=mamutal91
+  folder=GitHub
+  if [[ ${i} == @("infra"|"manifest"|"docs") ]]; then
+    account=AOSPK
+    folder=AOSPK
   fi
-
-  git clone ssh://git@github.com/${ACCOUNT}/${i} $HOME/${FOLDER}/${i}
+  git clone ssh://git@github.com/${account}/${i} $HOME/${folder}/${i}
 done
 
-git clone ssh://git@github.com/BuildersBR/buildersbr $HOME/${FOLDER}/buildersbr
+git clone ssh://git@github.com/BuildersBR/buildersbr $HOME/${folder}/buildersbr
