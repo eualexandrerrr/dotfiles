97fab88921b15b0e660a2aaf31b1ac65b540c7c8
Alexandre Rangel <mamutal91@gmail.com>
2021-06-12 08:15:54 -0300

    Remove push
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/kraken/create_repos_devices_and_invite.sh
+++ home/.config/kraken/create_repos_devices_and_invite.sh
@@ -4,10 +4,6 @@ clear
 
 source $HOME/.colors &>/dev/null
 
-if [[ -z ${1} ]]; then
-  echo -e "${BOL_YEL}You can: usage: command <vendor|push>${END}\n"
-fi
-
 workingDir=$(mktemp -d) && cd $workingDir
 
 echo -e "${BOL_CYA}Cloning official_devices${END}"
@@ -15,16 +11,6 @@ git clone https://github.com/AOSPK/official_devices &>/dev/null
 
 jsonDevices=${workingDir}/official_devices/devices.json
 jsonMaintainers=${workingDir}/official_devices/team/maintainers.json
-
-# GitLab vendors
-if [[ ${1} = vendor ]]; then
-  for vendorRepo in  $(jq '.[] | select(.name|test("^")) | .repositories' $jsonDevices | tr -d '"[]' | grep vendor); do
-    rm -rf $vendorRepo && mkdir $vendorRepo
-    cd $vendorRepo
-    glab repo create AOSPK-Devices/${vendorRepo} --defaultBranch eleven --public
-    cd ..
-  done
-fi
 
 # Loop
 for maintainer in $(jq '.[] | select(.name|test("^")) | .github_username' $jsonMaintainers | tr -d '"'); do
@@ -35,53 +21,15 @@ for maintainer in $(jq '.[] | select(.name|test("^")) | .github_username' $jsonM
       nameDevice=$(jq -r ".[] | select(.codename == \"${device}\") | .name" $jsonDevices)
 
       if grep -q "common\|kernel\|vendor" <<<"$repo"; then
-        continue
-      fi
-
-      if grep -q "common\|kernel\|vendor" <<<"$repo"; then
         gh repo create AOSPK-Devices/${repo} --public --confirm &>/dev/null
       else
         gh repo create AOSPK-Devices/${repo} -d "${brandDevice} ${nameDevice} maintained by @${maintainer}" --public --confirm &>/dev/null
       fi
 
-      echo -e "\n${BOL_GRE}Creating the repository${END} ${BLU}${device} #${CYA}${repo}${END} and inviting the ${YEL}@${maintainer}${END} maintainer"
+      echo -e "\n${BOL_GRE}Creating the repository${END} ${BLU}${device}\n# ${CYA}${repo}${END} and inviting the ${YEL}@${maintainer}${END} maintainer"
       echo -e "${RED}${brandDevice} $nameDevice${END}"
 
       gh api -X PUT repos/AOSPK-Devices/${repo}/collaborators/${maintainer} -f permission=admin &>/dev/null
-
-      if [[ ${1} = push ]]; then
-        rm -rf $repo
-        repoExist=$(git ls-remote --heads https://github.com/AOSPK-Devices/${repo} eleven)
-        if [[ -z ${repoExist} ]]; then
-          repoExist=$(git ls-remote --heads https://github.com/PixelExperience-Devices/${repo} eleven)
-          if [[ -z ${repoExist} ]]; then
-            echo "${BOL_YEL} * ~ PixelBlobs${END}"
-            git clone https://gitlab.pixelexperience.org/android/vendor-blobs/${repo} -b eleven
-          else
-            echo "${BOL_YEL} * ~ PixelExperience-Devices${END}"
-            git clone https://github.com/PixelExperience-Devices/${repo} -b eleven $repo
-          fi
-        else
-          echo "${BOL_YEL} * ~ AOSPK-Devices${END}"
-          git clone https://github.com/AOSPK-Devices/${repo} -b eleven $repo
-        fi
-
-        cd $repo
-
-        if grep -q "vendor" <<<"$repo"; then
-          glab repo create AOSPK-Devices/${repo} --defaultBranch eleven --public
-          if [[ $repo != vendor_xiaomi_mojito-vendor ]]; then
-            git push ssh://git@gitlab.com/AOSPK-Devices/${repo} HEAD:refs/heads/eleven --force
-          fi
-          git push ssh://git@github.com/AOSPK-Devices/${repo} HEAD:refs/heads/eleven --force
-        else
-          git push ssh://git@github.com/AOSPK-Devices/${repo} HEAD:refs/heads/eleven --force
-        fi
-
-        git push ssh://git@github.com/AOSPK-Devices/${repo} HEAD:refs/heads/eleven --force
-        cd ..
-      fi
-
     done
   done < <(jq -r ".[] | select(.github_username == \"${maintainer}\") | .devices[].codename" $jsonMaintainers)
 done
