8cb3dcf0cf6e8fc99275eaa687a2a1544a379779
Alexandre Rangel <mamutal91@gmail.com>
2021-06-12 04:39:10 -0300

    Cleaning up repositories
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/scripts/commiter.sh
+++ home/.config/scripts/commiter.sh
@@ -7,39 +7,15 @@ kraken=$HOME/AOSPK
 github=$HOME/GitHub
 
 repo1="dotfiles"
-repo2="mamutal91.github.io"
-repo3="myarch"
-repo4="infra"
-repo5="language-swaywm"
-repo6="shellscript-atom-snippets"
-repo7="AOSPK/manifest"
-repo8="Poco F2/device_xiaomi_lmi"
-repo9="Poco F2/device_xiaomi_sm8250-common"
-repo10="BuildersBR/buildersbr"
+repo2="myarch"
 
-repos="$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7\n$repo8\n$repo9\n$repo10"
+repos="${repo1}\n${repo2}"
 
-chosen="$(echo -e "$repos" | wofi --lines 10 --sort-order=DEFAULT --dmenu -p "  Commiter")"
+chosen="$(echo -e "$repos" | wofi --lines 5 --sort-order=DEFAULT --dmenu -p "  Commiter")"
 case $chosen in
     $repo1)
         alacritty -t newcommit --working-directory $HOME/.dotfiles;;
     $repo2)
         alacritty -t newcommit --working-directory $github/mamutal91.github.io;;
-    $repo3)
-        alacritty -t newcommit --working-directory $github/myarch;;
-    $repo4)
-        alacritty -t newcommit --working-directory $kraken/infra;;
-    $repo5)
-        alacritty -t newcommit --working-directory $github/language-swaywm;;
-    $repo6)
-        alacritty -t newcommit --working-directory $github/shellscript-atom-snippets;;
-    $repo7)
-        alacritty -t newcommit --working-directory $kraken/manifest;;
-    $repo8)
-        alacritty -t newcommit --working-directory $github/device_xiaomi_lmi;;
-    $repo9)
-        alacritty -t newcommit --working-directory $github/device_xiaomi_sm8250-common;;
-    $repo10)
-        alacritty -t newcommit --working-directory $github/buildersbr;;
 esac
 exit 0;
--- home/.config/scripts/repositories.sh
+++ home/.config/scripts/repositories.sh
@@ -3,28 +3,10 @@
 rm -rf $HOME/AOSPK && mkdir -p $HOME/AOSPK
 rm -rf $HOME/GitHub && mkdir -p $HOME/GitHub
 
-repos=(
-  mamutal91.github.io
-  myarch
-  userge-x
-  custom-rom
-  zsh-history
-  language-swaywm
-  shellscript-atom-snippets
-  manifest
-  docs
-  scripttest
-)
+cd $HOME
 
-for i in "${repos[@]}"; do
-  account=mamutal91
-  folder=GitHub/${i}
-  if [[ ${i} == @("infra"|"manifest"|"docs") ]]; then
-    account=AOSPK
-    folder=AOSPK
-  fi
-  if [[ ${i} = scripttest ]]; then
-    folder=.scripttest
-  fi
-  git clone ssh://git@github.com/${account}/${i} $HOME/${folder}
-done
+rm -rf $HOME/.scripttest
+
+git clone ssh://github.com/mamutal91/myarch GitHub/myarch
+git clone ssh://github.com/mamutal91/zsh-history GitHub/zsh-history
+git clone ssh://github.com/mamutal91/scripttest $HOME/.scripttest
