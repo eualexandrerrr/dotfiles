ead0102814805ae018a6440c3515405ac81b1320
Alexandre Rangel <mamutal91@gmail.com>
2021-06-15 14:37:13 -0300

    Adding make clean
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- home/.config/scripts/myMenu.sh
+++ home/.config/scripts/myMenu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 github=$HOME/GitHub
-
+# depois 7
 repo01="01.   dotfiles"
 repo02="02.   infra"
 repo03="03.   build ( lmi )"
@@ -9,19 +9,20 @@ repo04="04.   build ( random )"
 repo05="05.   build and sync"
 repo06="06.   build, sync and make clean"
 repo07="07.   build, sync and make installclean"
-repo08="09.   only sync"
-repo09="09.   stop/parar"
-repo10="10.   stop and clear/limpar queue"
-repo11="11.   enable-jobs"
-repo12="12.   disable-jobs"
-repo13="13.   myarch"
-repo14="14.   mytokens"
-repo15="15.   custom-rom"
-repo16="16.   shellscript-atom-snippets"
+repo08="08.   make clean"
+repo09="09.   only sync"
+repo10="10.   stop/parar"
+repo11="11.   stop and clear/limpar queue"
+repo12="12.   enable-jobs"
+repo13="13.   disable-jobs"
+repo14="14.   myarch"
+repo15="15.   mytokens"
+repo16="16.   custom-rom"
+repo17="17.   shellscript-atom-snippets"
 
-menu="$repo01\n$repo02\n$repo03\n$repo04\n$repo05\n$repo06\n$repo07\n$repo08\n$repo09\n$repo10\n$repo11\n$repo12\n$repo13\n$repo14\n$repo15\n$repo16"
+menu="$repo01\n$repo02\n$repo03\n$repo04\n$repo05\n$repo06\n$repo07\n$repo08\n$repo09\n$repo10\n$repo11\n$repo12\n$repo13\n$repo14\n$repo15\n$repo16\n$repo17"
 
-chosen="$(echo -e "$menu" | wofi --lines 16 --sort-order=alphabetical --dmenu -p "  myMenu")"
+chosen="$(echo -e "$menu" | wofi --lines 17 --sort-order=alphabetical --dmenu -p "  myMenu")"
 case $chosen in
   $repo01) alacritty -t mywindowfloat --working-directory $HOME/.dotfiles;;
   $repo02) alacritty -t mywindowfloat --working-directory $github/infra;;
@@ -30,14 +31,15 @@ case $chosen in
   $repo05) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh buildSync;;
   $repo06) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh buildSyncClean;;
   $repo07) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh buildSyncInstallclean;;
-  $repo08) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopOnlySync;;
-  $repo09) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopJobs;;
-  $repo10) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopClear;;
-  $repo11) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh enableJobs;;
-  $repo12) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh disableJobs;;
-  $repo13) alacritty -t mywindowfloat --working-directory $github/myarch;;
-  $repo14) alacritty -t mywindowfloat --working-directory $github/mytokens;;
-  $repo15) alacritty -t mywindowfloat --working-directory $github/custom-rom;;
-  $repo16) alacritty -t mywindowfloat --working-directory $github/shellscript-atom-snippets;;
+  $repo08) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh makeClean;;
+  $repo09) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopOnlySync;;
+  $repo10) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopJobs;;
+  $repo11) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopClear;;
+  $repo12) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh enableJobs;;
+  $repo13) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh disableJobs;;
+  $repo14) alacritty -t mywindowfloat --working-directory $github/myarch;;
+  $repo15) alacritty -t mywindowfloat --working-directory $github/mytokens;;
+  $repo16) alacritty -t mywindowfloat --working-directory $github/custom-rom;;
+  $repo17) alacritty -t mywindowfloat --working-directory $github/shellscript-atom-snippets;;
 esac
 exit 0;
--- home/.config/scripts/runCI.sh
+++ home/.config/scripts/runCI.sh
@@ -10,6 +10,7 @@ buildRandom="$cli build KrakenDev -p codename=mojito"
 buildSync="$cli build KrakenDev -p sync=true"
 buildSyncClean="$cli build KrakenDev -p sync=true -p mka_clean=true"
 buildSyncInstallclean="$cli build KrakenDev -p sync=true -p mka_installclean=true"
+makeClean="$cli build KrakenDev -p build=false -p sync=false -p mka_clean=true -p mka_installclean=false -p publishBuild=false"
 onlySync="$cli build KrakenDev -p build=false -p sync=true -p mka_clean=false -p mka_installclean=false -p publishBuild=false"
 stop="$cli stop-builds"
 clear="$cli clear-queue"
@@ -23,12 +24,13 @@ echo -e "${BOL_BLU}Executing command via ci-cli...\n${END}${GRE}"
 [ ${1} = buildRandom ] && echo -e "4. Building dirty for ${BLU}mojito${END}...${RED}" && $buildRandom
 [ ${1} = buildSync ] && echo -e "5. Synchronizing source to build dirty...${RED}" && $buildSync
 [ ${1} = buildSyncClean ] && echo -e "6. Cleaning build to build...${RED}" && $buildSyncClean
-[ ${1} = buildSyncInstallclean ] && echo -e "7. cleaning apps to build...${RED}" && $buildSyncInstallclean
-[ ${1} = stopOnlySync ] && echo -e "8. Stopping jobs and synchronizing source...${RED}" && $stop KrakenDev && $onlySync
-[ ${1} = stopJobs ] && echo -e "9. Stopping KrakenDev job running...${RED}" && $stop KrakenDev
-[ ${1} = stopClear ] && echo -e "10. Stopping jobs and clearing jobs from the queue...${RED}" && $clear
-[ ${1} = enableJobs ] && echo -e "11. Enabling jobs...${RED}" && $enable Kraken && $enable PA
-[ ${1} = disableJobs ] && echo -e "12. Disabling jobs...${RED}" && $disable Kraken && $disable PA
+[ ${1} = buildSyncInstallclean ] && echo -e "7. Cleaning apps to build...${RED}" && $buildSyncInstallclean
+[ ${1} = makeClean ] && echo -e "8. Make clean...${RED}" && $makeClean
+[ ${1} = stopOnlySync ] && echo -e "9. Stopping jobs and synchronizing source...${RED}" && $stop KrakenDev && $onlySync
+[ ${1} = stopJobs ] && echo -e "10. Stopping KrakenDev job running...${RED}" && $stop KrakenDev
+[ ${1} = stopClear ] && echo -e "11. Stopping jobs and clearing jobs from the queue...${RED}" && $clear
+[ ${1} = enableJobs ] && echo -e "12. Enabling jobs...${RED}" && $enable Kraken && $enable PA
+[ ${1} = disableJobs ] && echo -e "13. Disabling jobs...${RED}" && $disable Kraken && $disable PA
 
 echo -e "${BOL_YEL}"
 echo -ne "#####                     (33%)\r"
