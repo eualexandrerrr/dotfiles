7f4c816c566b6676dc419be164c43aef80ee507e
Alexandre Rangel <mamutal91@gmail.com>
2021-06-18 18:23:24 -0300

    Welcome Elmyra
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

---
+++ home/.config/kraken/hal/elmyra.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source $HOME/.colors &> /dev/null
+
+workingDir=$(mktemp -d) && cd $workingDir
+
+function elmyra() {
+  git clone https://github.com/ProtonAOSP/android_packages_apps_ElmyraService -b eleven packages_apps_ElmyraService
+
+  cd $workingDir/packages_apps_ElmyraService
+  git push ssh://git@github.com/AOSPK/packages_apps_ElmyraService HEAD:refs/heads/eleven --force
+  git push ssh://git@github.com/AOSPK-DEV/packages_apps_ElmyraService HEAD:refs/heads/eleven --force
+}
+
+elmyra
+
+rm -rf $workingDir
