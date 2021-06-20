9bbbd1c7b7ede936bee9653e8a953244f6bf5cf9
Alexandre Rangel <mamutal91@gmail.com>
2021-06-19 22:39:49 -0300

    use one menu only
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- allscripts/.config/scripts/myMenu.sh
+++ allscripts/.config/scripts/myMenu.sh
@@ -8,11 +8,22 @@ repo03="03.   myarch"
 repo04="04.   mytokens"
 repo05="05.   custom-rom"
 repo06="06.   shellscript-atom-snippets"
-repo07="07.   ci | kraken"
 
-menu="$repo01\n$repo02\n$repo03\n$repo04\n$repo05\n$repo06\n$repo07"
+repo07="07.   build ( lmi )"
+repo08="08.   build ( random )"
+repo09="09.   build and sync"
+repo10="10.   build, sync and make clean"
+repo11="11.   build, sync and make installclean"
+repo12="12.   make clean"
+repo13="13.   only sync"
+repo14="14.   stop/parar"
+repo15="15.   stop and clear/limpar queue"
+repo16="16.   enable-jobs"
+repo17="17.   disable-jobs"
 
-chosen="$(echo -e "$menu" | wofi --lines 7 --sort-order=alphabetical --dmenu -p "  myMenu")"
+menu="$repo01\n$repo02\n$repo03\n$repo04\n$repo05\n$repo06\n$repo07\n$repo08\n$repo09\n$repo10\n$repo11\n$repo12\n$repo13\n$repo14\n$repo15\n$repo16\n$repo17"
+
+chosen="$(echo -e "$menu" | wofi --lines 17 --sort-order=alphabetical --dmenu -p "  myMenu")"
 case $chosen in
   $repo01) alacritty -t mywindowfloat --working-directory $HOME/.dotfiles ;;
   $repo02) alacritty -t mywindowfloat --working-directory $github/infra ;;
@@ -20,6 +31,17 @@ case $chosen in
   $repo04) alacritty -t mywindowfloat --working-directory $github/mytokens ;;
   $repo05) alacritty -t mywindowfloat --working-directory $github/custom-rom ;;
   $repo06) alacritty -t mywindowfloat --working-directory $github/shellscript-atom-snippets ;;
-  $repo07) $HOME/.config/scripts/ciMenu.sh & ;;
+
+  $repo07) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh build ;;
+  $repo08) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh buildRandom ;;
+  $repo09) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh buildSync ;;
+  $repo10) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh buildSyncClean ;;
+  $repo11) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh buildSyncInstallclean ;;
+  $repo12) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh makeClean ;;
+  $repo13) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopOnlySync ;;
+  $repo14) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopJobs ;;
+  $repo15) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh stopClear ;;
+  $repo16) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh enableJobs ;;
+  $repo17) alacritty -t mywindowfloat -e $HOME/.config/scripts/runCI.sh disableJobs ;;
 esac
 exit 0
