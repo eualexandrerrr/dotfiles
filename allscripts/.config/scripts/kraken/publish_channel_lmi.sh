19e9b914c97ebcf1668f58be736adf1af5baede3
Alexandre Rangel <mamutal91@gmail.com>
2021-06-20 02:24:18 -0300

    Fix personal post lmi
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- allscripts/.config/scripts/kraken/publish_channel_lmi.sh
+++ allscripts/.config/scripts/kraken/publish_channel_lmi.sh
@@ -1,49 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ -z ${1} ]]; then
-  echo "Você precisa definir o .zip, exemplo, ./publish_channel_lmi.sh Kraken-11-GApps-20210611-1919-lmi.zip"
-  exit
-else
-  zipBuild=${1}
-  clear
-fi
+zipBuild=${1}
 
-pwd=$(pwd)
+source /home/mamutal91/.myTokens
 
-source /home/mamutal91/.botTokens
-
-workingDir=$(mktemp -d) && cd $workingDir
-
+dateFull=$(echo ${zipBuild} | tr -d 'KrakenVanillaGAppszip' | cut -c6-13)
 year=$(echo ${dateFull} | cut -c3-4)
-month=$(echo ${dateFull} | cut -c6-7)
-day=$(echo ${dateFull} | cut -c9-10)
+month=$(echo ${dateFull} | cut -c4-5)
+day=$(echo ${dateFull} | cut -c5-6)
 
-changelogFile=$(basename $zipBuild .zip)
-changelogFile=${changelogFile}.txt
-
+echo $year
 img=/home/mamutal91/.dotfiles/home/.config/scripts/kraken/images/banner_lmi.jpg
 
 codename=lmi
-
-rm -rf $changelogFile
-wget https://raw.githubusercontent.com/AOSPK-DEV/official_devices/master/changelogs/${codename}/${changelogFile} &> /dev/null
-sed -i "s/^/• /" $changelogFile
-
-cat $changelogFile
-
-changelogSource="*Source changelog:*
-• Feature to enable/disable blur"
-
-changelogDevice="*Device changelog:*
-$(cat ${changelogFile})"
-
-if [[ -z $changelogSource ]]; then
-  changelogFull="$changelogDevice"
-else
-  changelogFull="$changelogSource
-
-$changelogDevice"
-fi
 
 msg="#AOSPK #KRAKEN #ROM #Official #R #lmi
 *The Kraken Project - OFFICIAL | Android 11.*
@@ -53,7 +22,6 @@ msg="#AOSPK #KRAKEN #ROM #Official #R #lmi
 ▪️[GApps variant](https://aospk.org/lmi) [1.5 GB]
 ▪️[Support](https://t.me/AOSPKChat)
 
-$changelogFull
 
 *Required firmware:*
 • V12.2.6.0.RJKMIXM
@@ -70,7 +38,3 @@ function sendMessage() {
     -F parse_mode="markdown"
 }
 sendMessage
-
-rm -rf $workingDir
-
-cd $pwd
