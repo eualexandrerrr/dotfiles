f73ec124ac61e11eab168eb300ce40884cac886d
Alexandre Rangel <mamutal91@gmail.com>
2021-06-27 15:17:19 -0300

    Fix typo
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/laptop-mode.sh
+++ setup/laptop-mode.sh
@@ -16,4 +16,4 @@ CONTROL_BRIGHTNESS=1
 BATT_BRIGHTNESS_COMMAND="echo 206"
 BATT_BRIGHTNESS_COMMAND="echo 206"
 NOBATT_BRIGHTNESS_COMMAND="echo 206"
-BRIGHTNESS_OUTPUT="/sys/class/backlight/amdgpu_bl0/brightness"'   | sudo tee /etc/laptop-mode/laptop-mode.conf
+BRIGHTNESS_OUTPUT="/sys/class/backlight/amdgpu_bl0/brightness"' | sudo tee /etc/laptop-mode/laptop-mode.conf
