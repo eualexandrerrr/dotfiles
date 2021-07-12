9aa1d786e9c0c92ca021add9be745d5ec4af01d1
Alexandre Rangel <mamutal91@aospk.org>
2021-07-12 16:02:45 -0300

    Command to see all available kernels from chaotic
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/personalconfigs/Xgamer-chaotic.sh
+++ setup/personalconfigs/Xgamer-chaotic.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pacman -Ss linux-tkg
+
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
