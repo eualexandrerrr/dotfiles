09077db3ab9b3dc8114899ad16b4acc0fd561db0
Alexandre Rangel <mamutal91@aospk.org>
2021-07-14 23:36:45 -0300

    capture background with maim
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- allscripts/.config/scripts/screenshot.sh
+++ allscripts/.config/scripts/screenshot.sh
@@ -7,11 +7,11 @@ dir="$HOME/Images/Screenshots/"
 [[ ! -d $dir ]] && mkdir -p $dir
 
 if [[ ${1} == "window" ]]; then
-  maim -s --format png "$dir/w-$date.png"
+  maim -s -B --format png "$dir/w-$date.png"
   xclip -selection clipboard -t image/png -i "$dir/w-$date.png"
   dunstify -i $icon "Screenshot" "Cropped capture.\n w-$date.png"
 else
-  maim --format png "$dir/f-$date.png"
+  maim -B --format png "$dir/f-$date.png"
   sleep 1
   xclip -selection clipboard -t image/png -i "$dir/f-$date.png"
   dunstify -i $icon "Screenshot" "Fullscreen capture.\n f-$date.png"
