b3d9b24e698d4689724cd010dc2d24bdf1081e5c
Alexandre Rangel <mamutal91@aospk.org>
2021-07-15 11:02:35 -0300

    Creating banner on official_devices
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- allscripts/.config/scripts/kraken/create_repos_devices_and_invite.sh
+++ allscripts/.config/scripts/kraken/create_repos_devices_and_invite.sh
@@ -8,7 +8,7 @@ source $HOME/.mytokens/.myTokens &> /dev/null
 workingDir=$HOME/GitHub
 
 workingDir=$(mktemp -d) && cd $workingDir
-git clone https://github.com/AOSPK/official_devices
+git clone ssh://git@github.com/AOSPK/official_devices
 
 jsonDevices=${workingDir}/official_devices/devices.json
 jsonMaintainers=${workingDir}/official_devices/team/maintainers.json
@@ -43,6 +43,41 @@ createRepo() {
   done
 }
 
+function generateBanner() {
+  echo "${BOL_BLU}Creating banner ${BOL_MAG}${codename}${END}"
+  bannerModel=${workingDir}/official_devices/images/banners/banner_model.png
+
+  outputTmp1=/tmp/bannertmp1_${codename}.png
+  outputTmp2=/tmp/bannertmp2_${codename}.png
+  outputTmp3=/tmp/${codename}.png
+
+  rm -rf $outputTmp1 $outputTmp2 $outputTmp3
+
+  ffmpeg -i $bannerModel -vf "drawtext=text='${brandDevice}':fontcolor=white:fontsize=28:x=170:y=363:" $outputTmp1 &> /dev/null
+  ffmpeg -i $outputTmp1 -vf "drawtext=text='${nameDevice}':fontcolor=white:fontsize=45:x=170:y=403:" $outputTmp2 &> /dev/null
+  ffmpeg -i $outputTmp2 -vf "drawtext=text='${maintainerGithub}':fontcolor=white:fontsize=30:x=230:y=456:" $outputTmp3 &> /dev/null
+
+  cp -rf /tmp/${codename}.png ${workingDir}/official_devices/images/banners/
+}
+
+gitCommit() {
+  pwd=$(pwd)
+  cd ${workingDir}/official_devices
+  status=$(git add . -n)
+  if [[ -n $status   ]]; then
+    echo -e "${BOL_GRE}Commit!${END}"
+    m="[KRAKEN-CI]: Updates"
+    git config --global user.email "bot@aospk.org" && git config --global user.name "Kraken Project Bot"
+    git add .
+    git commit -m "${m}" --signoff --author "Kraken Project Bot <bot@aospk.org>" --date "$(date)"
+    git push
+    git config --global user.email "mamutal91@aospk.org" && git config --global user.name "Alexandre Rangel"
+  else
+    echo ${BOL_MAG}No commits!${END}
+  fi
+  cd $pwd
+}
+
 for codename in $(jq '.[] | select(.name|test("^")) | .codename' $jsonDevices | tr -d '"'); do
 
     brandDevice=$(jq -r ".[] | select(.codename == \"${codename}\") | .brand" $jsonDevices)
@@ -58,13 +93,16 @@ for codename in $(jq '.[] | select(.name|test("^")) | .codename' $jsonDevices | 
 
     if [[ $deprecated == true ]]; then
       echo -e "${BOL_BLU}${BLU}$brandDevice $nameDevice ${BOL_MAG}($codename)${END} ${BOL_RED}DEPRECATED!!!${END}"
+      rm -rf ${workingDir}/official_devices/images/banners/${codename}.png
     else
       echo -e "${BOL_BLU}${BLU}$brandDevice $nameDevice ${BOL_MAG}($codename)${END}"
       echo -e "${BOL_GRE}${GRE}$maintainerName ($maintainerGithub)"
       echo -e "${BOL_YEL}Repositories:\n${YEL}$repositories"
       createRepo
+      generateBanner
     fi
 
     echo -e "${BOL_CYA}\n--------- END ---------${END}\n"
-
 done
+
+gitCommit
