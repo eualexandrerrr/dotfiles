401623c147baa37bf3d86812ff126872c2778a24
Alexandre Rangel <mamutal91@aospk.org>
2021-07-25 17:13:49 -0300

    Return with old IP
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- allscripts/.config/scripts/runCI.sh
+++ allscripts/.config/scripts/runCI.sh
@@ -9,7 +9,7 @@ if [[ ! -f $HOME/.jenkins-cli.jar ]]; then
   mv jenkins-cli.jar .jenkins-cli.jar
 fi
 
-cli="java -jar $HOME/.jenkins-cli.jar -s http://65.21.193.29:8090 -auth ${myUserCI}:${ciKrakenToken} -webSocket"
+cli="java -jar $HOME/.jenkins-cli.jar -s http://86.109.7.111:8080 -auth ${myUserCI}:${ciKrakenToken} -webSocket"
 
 JOB=KrakenDev
 codename=lmi
