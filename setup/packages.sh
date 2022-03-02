d3a4bbbac6caae0557f27fae1b560b4be6b2c443
Alexandre Rangel <mamutal91@gmail.com>
2022-03-02 00:57:41 -0300

    Drop zen kernel
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/packages.sh
+++ setup/packages.sh
@@ -26,9 +26,6 @@ mypackages=(
   archlinux-keyring gnupg cronie net-tools
   noip
 
-  # ZenKernel
-  linux-zen linux-zen-headers
-
   # Video/games
   nvidia-lts nvidia-utils nvidia-settings nvidia-utils nvidia-dkms nvidia-prime opencl-nvidia nbfc-git
   mesa mesa-demos vulkan-tools lib32-nvidia-utils lib32-opencl-nvidia lib32-virtualgl lib32-nvidia-utils lib32-libvdpau lib32-opencl-nvidia lib32-mesa
--- setup/personalconfigs/6-chaotic.sh
+++ setup/personalconfigs/6-chaotic.sh
@@ -12,7 +12,7 @@ sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key FBA220DFC880C036
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-sudo pacman -Sy --no-confirm && sudo powerpill -Su --no-confirm && paru -Su --no-confirm
+sudo pacman -Sy --noconfirm && sudo powerpill -Su --noconfirm && paru -Su --noconfirm
 
 echo "[chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
@@ -23,8 +23,7 @@ sudo pacman -Sy chaotic-aur/dxvk-mingw-git --noconfirm
 
 sudo pacman -Ss linux- | grep chaotic-aur
 
-kernel=linux-tkg-pds
-kernel=linux-xanmod-cacule
+kernel=linux-tkg-cfs-generic_v3
 
 sudo pacman -S "${kernel}" --noconfirm
 sudo pacman -S "${kernel}-headers" --noconfirm
