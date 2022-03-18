7b3091556fe91d5d94249ada7014bd3cc2826ed7
Alexandre Rangel <mamutal91@gmail.com>
2022-03-18 20:44:05 -0300

    Fix nbfc
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/nbfc.sh
+++ setup/nbfc.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 sudo cp -rf $HOME/.dotfiles/assets/.config/assets/fans/Acer\ Nitro\ 5\ AN515-43.xml /opt/nbfc/Configs
-nbfc config -a "Acer Nitro 5 AN515-43"
 
-nbfc
 sudo systemctl enable nbfc
 sudo systemctl start nbfc
+
+nbfc config -a "Acer Nitro 5 AN515-43"
