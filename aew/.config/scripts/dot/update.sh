8222d963517b2e9590674dff6d3c4f191b2928da
Alexandre Rangel <mamutal91@gmail.com>
2022-03-18 19:43:56 -0300

    Log pacman update
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- aew/.config/scripts/dot/update.sh
+++ aew/.config/scripts/dot/update.sh
@@ -6,20 +6,27 @@ icon="$HOME/.config/assets/icons/pacman.png"
 
 clear
 
-echo -e "${BOL_GRE}Updating packages${END}"
-sudo pacman -Syu
-yay -Syu
+updatePackages() {
+  date=$(date +"%Y%m%d-%H%M")
 
-if sudo pacman -Qdtq; then
-  echo -e "${BOL_RED}Removing unused packages${END}"
-  sudo pacman -Rncs $(sudo pacman -Qdtq) --noconfirm
-  yay -Rncs $(yay -Qdtq) --noconfirm
-else
-  echo -e "${BOL_BLU}There are no unused packages to remove${END}"
-fi
+  echo -e "${BOL_GRE}Updating packages ${BOL_RED}${date}${END}"
+  sudo pacman -Syu
+  yay -Syu
 
-echo -e "\n${BOL_RED}Removing /tmp/ files... ${BOL_GRE}($(sudo du -sh /tmp | awk '{ print $1 }'))${END}"
-sudo rm -rf /tmp/* &> /dev/null
+  if sudo pacman -Qdtq; then
+    echo -e "${BOL_RED}Removing unused packages${END}"
+    sudo pacman -Rncs $(sudo pacman -Qdtq) --noconfirm
+    yay -Rncs $(yay -Qdtq) --noconfirm
+  else
+    echo -e "${BOL_BLU}There are no unused packages to remove${END}"
+  fi
 
-play $HOME/.config/assets/sounds/completed.wav &> /dev/null
-dunstify -i $icon "ArchLinux" "Successfully updated packages."
+  echo -e "\n${BOL_RED}Removing /tmp/ files... ${BOL_GRE}($(sudo du -sh /tmp | awk '{ print $1 }'))${END}"
+  echo -e "${BOL_GRE}Packages finished ${BOL_RED}${date}${END}"
+  sudo rm -rf /tmp/* &> /dev/null
+
+  play $HOME/.config/assets/sounds/completed.wav &> /dev/null
+  dunstify -i $icon "ArchLinux" "Successfully updated packages."
+}
+
+updatePackages | tee -a $HOME/.pacman_logs/$date
