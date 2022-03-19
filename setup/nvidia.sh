d09fb0613c8f5a5202f0352b1ac5236cfe4cadbb
Alexandre Rangel <mamutal91@gmail.com>
2022-03-19 18:01:33 -0300

    Function to install nvidia driver
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/nvidia.sh
+++ setup/nvidia.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
 
-# graphics driver
-if [[ ! -n $(grep nvidia /etc/default/grub) ]]; then
-  sudo sed -i 's/acpi_backlight=vendor/acpi_backlight=vendor nvidia-drm.modeset=1/g' /etc/default/grub
-fi
-
-if [[ ! -n $(grep nvidia /etc/mkinitcpio.conf) ]]; then
-  sudo sed -i "s/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g" /etc/mkinitcpio.conf
-fi
-
-pwd=$(pwd)
-  rm -rf /tmp/nvidia-all
-  mkdir -p /tmp
-  git clone https://github.com/Frogging-Family/nvidia-all.git /tmp/nvidia-all
-  cd /tmp/nvidia-all
-  makepkg -si
-  sudo pacman -Sy mesa mesa-demos vulkan-tools lib32-mesa lib32-virtualgl lib32-libvdpau --noconfirm
-  rm -rf /tmp/nvidia-all
-  sudo mkinitcpio -P
-cd $pwd
-
 sudo rm -rf /etc/X11/xorg.conf.d/20-nvidia-drm-outputclass.conf
 sudo rm -rf /etc/modprobe.d/blacklist-nvidia-nouveau.conf
 
@@ -60,3 +40,26 @@ Section "OutputClass"
     ModulePath "/usr/lib/nvidia/xorg"
     ModulePath "/usr/lib/xorg/modules"
 EndSection' | sudo tee /etc/X11/xorg.conf.d/20-nvidia-drm-outputclass.conf &> /dev/null
+
+# graphics driver
+if [[ ! -n $(grep nvidia /etc/default/grub) ]]; then
+  sudo sed -i 's/acpi_backlight=vendor/acpi_backlight=vendor nvidia-drm.modeset=1/g' /etc/default/grub
+fi
+
+if [[ ! -n $(grep nvidia /etc/mkinitcpio.conf) ]]; then
+  sudo sed -i "s/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g" /etc/mkinitcpio.conf
+fi
+
+installNvidia() {
+  pwd=$(pwd)
+    sudo pacman -Sy mesa mesa-demos vulkan-tools lib32-mesa lib32-virtualgl lib32-libvdpau --noconfirm
+    rm -rf $HOME/nvidia-all
+    git clone https://github.com/Frogging-Family/nvidia-all.git $HOME/nvidia-all
+    cd $HOME/nvidia-all
+    makepkg -si
+    rm -rf $HOME/nvidia-all
+    sleep 200000
+    sudo mkinitcpio -P
+  cd $pwd
+}
+installNvidia
