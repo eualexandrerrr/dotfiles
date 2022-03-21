627b010b7f8541060780198fe7976f489d098e0f
Alexandre Rangel <mamutal91@gmail.com>
2022-03-20 23:56:04 -0300

    nbfc with xml
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- setup/nbfc.sh
+++ setup/nbfc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo cp -rf $HOME/.dotfiles/assets/.config/assets/fans/Acer\ Nitro\ 5\ AN515-43.json /usr/share/nbfc/configs/
+sudo cp -rf $HOME/.dotfiles/assets/.config/assets/fans/Acer\ Nitro\ 5\ AN515-43.xml /opt/nbfc/Configs
 
 nbfcService="nbfc"
 
