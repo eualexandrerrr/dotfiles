e8de1440d8830d3f7a80e998f62075e9f69a3978
Alexandre Rangel <mamutal91@gmail.com>
2022-03-21 22:19:34 -0300

    Arrow's Hall
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- aew/.config/scripts/kraken/hal/hal.sh
+++ aew/.config/scripts/kraken/hal/hal.sh
@@ -13,6 +13,7 @@ repos=(
 branchBase="arrow-12.1-caf-${1}"
 branchKraken="twelve-caf-${1}"
 branchBaseLos="lineage-19.1-caf-${1}"
+branchBaseLosOld="lineage-19.0-caf-${1}"
 
 for i in ${repos[@]}; do
   rm -rf ${i}
@@ -21,12 +22,16 @@ for i in ${repos[@]}; do
   echo -e "${BOL_GRE}LineageOS : ${BOL_YEL}${branchBaseLos}${END}"
   echo -e "${BOL_CYA}Kraken    : ${BOL_MAG}${branchKraken}${END}"
 
-  git clone https://github.com/LineageOS/android_${i} -b ${branchBaseLos} ${i}
-#  git clone https://github.com/ArrowOS/android_${i} -b ${branchBase} ${i}
+  git clone https://github.com/ArrowOS/android_${i} -b ${branchBase} ${i}
   if [ $? -eq 0 ]; then
     echo "${BOL_GRE}Success${END}"
   else
     git clone https://github.com/LineageOS/android_${i} -b ${branchBaseLos} ${i}
+    if [ $? -eq 0 ]; then
+      echo "${BOL_GRE}Success${END}"
+    else
+      git clone https://github.com/LineageOS/android_${i} -b ${branchBaseLosOld} ${i}
+    fi
   fi
 
   cd ${i}
