84d3c3f822a60ffc16e77bd828f5d201377b45f4
Alexandre Rangel <mamutal91@gmail.com>
2022-03-21 23:17:59 -0300

    Hals script changes
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- aew/.config/scripts/kraken/hal/hal.sh
+++ aew/.config/scripts/kraken/hal/hal.sh
@@ -16,29 +16,44 @@ branchBaseLos="lineage-19.1-caf-${1}"
 branchBaseLosOld="lineage-19.0-caf-${1}"
 
 for i in ${repos[@]}; do
+
+#  [[ $branchBase == "arrow-12.1-caf-msm8998" ]] && continue
+#  [[ $branchBase == "arrow-12.1-caf-msm8996" ]] && continue
+#  [[ $branchBase == "arrow-12.1-caf-sdm660" ]] && continue
+#  [[ $branchBase == "arrow-12.1-caf-sdm845" ]] && continue
+#  [[ $branchBase == "arrow-12.1-caf-sm8150" ]] && continue
+#  [[ $branchBase == "arrow-12.1-caf-sm8250" ]] && continue
+
+#  [[ $i == "hardware_qcom_audio" ]] && continue
+#  [[ $i == "hardware_qcom_display" ]] && continue
+#  [[ $i == "hardware_qcom_media" ]] && continue
+
   rm -rf ${i}
   echo -e "${BOL_GRE}Repo      : ${BOL_YEL}${i}${END}"
   echo -e "${BOL_GRE}ArrowOS   : ${BOL_YEL}${branchBase}${END}"
   echo -e "${BOL_GRE}LineageOS : ${BOL_YEL}${branchBaseLos}${END}"
-  echo -e "${BOL_CYA}Kraken    : ${BOL_MAG}${branchKraken}${END}"
+  echo -e "${BOL_CYA}Kraken    : ${BOL_MAG}${branchKraken}${END}\n"
 
-  git clone https://github.com/ArrowOS/android_${i} -b ${branchBase} ${i}
+  git clone https://github.com/ArrowOS/android_${i} -b ${branchBase} ${i} --single-branch
   if [ $? -eq 0 ]; then
-    echo "${BOL_GRE}Success${END}"
+    echo -e "\n${BOL_GRE}ArrowOS ${branchBase} ${BOL_CYA}cloned success${END}"
   else
-    git clone https://github.com/LineageOS/android_${i} -b ${branchBaseLos} ${i}
+    git clone https://github.com/LineageOS/android_${i} -b ${branchBaseLos} ${i} --single-branch
     if [ $? -eq 0 ]; then
-      echo "${BOL_GRE}Success${END}"
+      echo -e "\n${BOL_GRE}LineageOS ${branchBaseLos} ${BOL_CYA}cloned success${END}"
     else
-      git clone https://github.com/LineageOS/android_${i} -b ${branchBaseLosOld} ${i}
+      git clone https://github.com/LineageOS/android_${i} -b ${branchBaseLosOld} ${i} --single-branch
+      echo -e "\n${BOL_GRE}LineageOS ${branchBaseLosOld} ${BOL_CYA}cloned success${END}"
     fi
   fi
 
   cd ${i}
+  echo -e "\n${BOL_CYA}Pushing to org ${BOL_MAG}AOSPK${END}"
   git push ssh://git@github.com/AOSPK/${i} HEAD:refs/heads/${branchKraken} --force
+  echo -e "\n${BOL_CYA}Pushing to org ${BOL_MAG}AOSPK-Next${END}\n"
   git push ssh://git@github.com/AOSPK-Next/${i} HEAD:refs/heads/${branchKraken} --force
 
-  echo -e "${BOL_GRE}-----------------------------------------------"
+  echo -e "\n${GRE}-----------------------------------------------"
 done
 
 rm -rf $workingDir
