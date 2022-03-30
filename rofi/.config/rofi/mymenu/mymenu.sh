fbd513a8e3fa90e2b2a4d0bacd99760ec43da48d
Alexandre Rangel <mamutal91@gmail.com>
2022-03-30 01:52:47 -0300

    Put vendor_aosp in mymenu
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- rofi/.config/rofi/mymenu/mymenu.sh
+++ rofi/.config/rofi/mymenu/mymenu.sh
@@ -7,8 +7,9 @@ repo02="02.   infra / docker-files"
 repo03="03.   myarch"
 repo04="04.   mytokens"
 repo05="05.   shellscript-atom-snippets"
+repo06="06.   vendor_aosp"
 
-menu="$repo01\n$repo02\n$repo03\n$repo04\n$repo05"
+menu="$repo01\n$repo02\n$repo03\n$repo04\n$repo05\n$repo06"
 
 chosen="$(echo -e "$menu" | rofi -dmenu -theme $HOME/.config/rofi/mymenu/style.rasi)"
 
@@ -18,5 +19,6 @@ case $chosen in
   $repo03) alacritty -t mywindowfloat --working-directory $github/myarch ;;
   $repo04) alacritty -t mywindowfloat --working-directory $github/mytokens ;;
   $repo05) alacritty -t mywindowfloat --working-directory $github/shellscript-atom-snippets ;;
+  $repo06) alacritty -t mywindowfloat --working-directory $HOME/Kraken/vendor/aosp ;;
 esac
 exit 0
