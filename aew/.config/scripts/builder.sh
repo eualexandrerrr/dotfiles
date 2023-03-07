18fdb6f89d1d62c91f84ccaa593fd75af56cc1f1
Alexandre Rangel <mamutal91@gmail.com>
2023-03-07 09:53:57 -0300

    Archiso and builder script adaptations
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- aew/.config/scripts/builder.sh
+++ aew/.config/scripts/builder.sh
@@ -20,7 +20,7 @@ sideload() {
 }
 
 tree() {
-  cd $HOME/PixelStuffs
+  cd $HOME/Evox
   echo $PWD
   rm -rf device/xiaomi hardware/xiaomi vendor/xiaomi kernel/xiaomi
   git clone ssh://git@github.com/mamutal91/device_xiaomi_veux -b thirteen device/xiaomi/veux
@@ -29,7 +29,7 @@ tree() {
 
   git clone https://github.com/ghostrider-reborn/android_kernel_xiaomi_lisa -s -b sapphire kernel/xiaomi/sm6375
 
-  git clone ssh://git@github.com/PixelExperience/hardware_xiaomi -b thirteen hardware/xiaomi
+  git clone ssh://git@github.com/Evolution-X/hardware_xiaomi -b tiramisu hardware/xiaomi
 }
 
 ccacheC() {
@@ -43,17 +43,17 @@ ccacheC() {
 }
 
 s() {
-  mkdir -p $HOME/PixelStuffs
-  cd $HOME/PixelStuffs
-  echo -e "${GRE}PixelStuffs${END} $PWD"
-  repo init -u https://github.com/PixelStuffs/manifest -b thirteen
+  mkdir -p $HOME/Evox
+  cd $HOME/Evox
+  echo -e "${GRE}Evox${END} $PWD"
+  repo init -u https://github.com/Evolution-X/manifest -b tiramisu
   repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)
 }
 
 c() {
   ccacheC
-  cd $HOME/PixelStuffs
-  echo -e "${GRE}PixelStuffs${END} $PWD"
+  cd $HOME/Evox
+  echo -e "${GRE}Evox${END} $PWD"
 
   cp -rf log.txt old_log.txt &> /dev/null
 
@@ -62,6 +62,6 @@ c() {
 
   sleep 10
   mkdir -p $HOME/Builds
-  mv $HOME/PixelStuffs/out/target/product/veux/Arrow*.zip $HOME/Builds
-  rm -rf $HOME/PixelStuffs/out/target/product/veux/Arrow*
+  mv $HOME/Evox/out/target/product/veux/Evo*.zip $HOME/Builds
+  rm -rf $HOME/Evox/out/target/product/veux/Evo*
 }
